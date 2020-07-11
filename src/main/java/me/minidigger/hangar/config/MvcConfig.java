aa9f3d499150a15c89613e1488dde3d3808fb180
MiniDigger <admin@minidigger.me>
2020-07-11 17:52:48 +0200

    properly serve public folder

--- src/main/java/me/minidigger/hangar/config/MvcConfig.java
+++ src/main/java/me/minidigger/hangar/config/MvcConfig.java
@@ -2,11 +2,16 @@ package me.minidigger.hangar.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.CacheControl;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.freemarker.FreeMarkerConfigurer;
 import org.springframework.web.servlet.view.freemarker.FreeMarkerViewResolver;
 
+import java.util.concurrent.TimeUnit;
+
 @Configuration
-public class MvcConfig {
+public class MvcConfig implements WebMvcConfigurer {
 
     @Bean
     public FreeMarkerViewResolver freemarkerViewResolver() {
@@ -22,5 +27,12 @@ public class MvcConfig {
         FreeMarkerConfigurer freeMarkerConfigurer = new FreeMarkerConfigurer();
         freeMarkerConfigurer.setTemplateLoaderPath("classpath:templates");
         return freeMarkerConfigurer;
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/assets/**")
+                .addResourceLocations("classpath:/public/")
+                .setCacheControl(CacheControl.maxAge(1, TimeUnit.DAYS));
     }
 }
