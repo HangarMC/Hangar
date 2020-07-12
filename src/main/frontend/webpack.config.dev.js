fab89436f4d4e79a751cfe67964a053ac199e11b
Keimeno <constantin@metzworld.com>
2020-07-12 12:06:06 +0200

    added development environment with hot reload

--- src/main/frontend/webpack.config.dev.js
+++ src/main/frontend/webpack.config.dev.js
@@ -2,5 +2,6 @@ const merge = require("webpack-merge");
 const commonConfig = require("./webpack.config.common.js");
 
 module.exports = merge.merge(commonConfig, {
-    mode: "development"
+    mode: "development",
+    watch: true
 });
