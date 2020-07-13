019d299142f89653a3ac9cad3365f87877b74ef8
MiniDigger <admin@minidigger.me>
2020-07-13 22:53:18 +0200

    add missing routes

--- src/main/java/me/minidigger/hangar/util/RouteHelper.java
+++ src/main/java/me/minidigger/hangar/util/RouteHelper.java
@@ -36,10 +36,62 @@ public class RouteHelper {
         register("userAdmin", "/admin/user/{user}", of("user"), of());
         register("javaScriptRoutes", "/javascriptRoutes", of(), of());
 
+        register("projects.rename", "/{author}/{slug}/manage/rename", of("author", "slug"), of());
+        register("projects.setWatching", "/{author}/{slug}/watchers/{watching}", of("author", "slug", "watching"), of());
+        register("projects.showSettings", "/{author}/{slug}/manage", of("author", "slug"), of());
+        register("projects.setInviteStatus", "/invite/{id}/{status}", of("id", "status"), of());
+        register("projects.toggleStarred", "/{author}/{slug}/stars/toggle", of("author", "slug"), of());
         register("projects.showCreator", "/new", of(), of());
-        //TODO
-        //GET     /:author/:slug/stars                                        @controllers.project.Projects.showStargazers(author, slug, page: Option[Int])
-        //GET     /:author/:slug/watchers                                     @controllers.project.Projects.showWatchers(author, slug, page: Option[Int])
+        register("projects.showStargazers", "/{author}/{slug}/stars", of("author", "slug"), of("page"));
+        register("projects.showWatchers", "/{author}/{slug}/watchers", of("author", "slug"), of("page"));
+        register("projects.uploadIcon", "/{author}/{slug}/icon", of("author", "slug"), of());
+        register("projects.showIcon", "/{author}/{slug}/icon", of("author", "slug"), of());
+        register("projects.sendForApproval", "/{author}/{slug}/manage/sendforapproval", of("author", "slug"), of());
+        register("projects.setInviteStatusOnBehalf", "/invite/{id}/{status}/{behalf}", of("id", "status", "behalf"), of());
+        register("projects.delete", "/{author}/{slug}/manage/hardDelete", of("author", "slug"), of());
+        register("projects.addMessage", "/{author}/{slug}/notes/addmessage", of("author", "slug"), of());
+        register("projects.showPendingIcon", "/{author}/{slug}/icon", of("author", "slug"), of());
+        register("projects.postDiscussionReply", "/{author}/{slug}/discuss/reply", of("author", "slug"), of());
+        register("projects.show", "/{author}/{slug}", of("author", "slug"), of());
+        register("projects.showDiscussion", "/{author}/{slug}/discuss", of("author", "slug"), of());
+        register("projects.softDelete", "/{author}/{slug}/manage/delete", of("author", "slug"), of());
+        register("projects.showFlags", "/{author}/{slug}/flags", of("author", "slug"), of());
+        register("projects.flag", "/{author}/{slug}/flag", of("author", "slug"), of());
+        register("projects.createProject", "/", of(), of());
+        register("projects.resetIcon", "/{author}/{slug}/icon/reset", of("author", "slug"), of());
+        register("projects.save", "/{author}/{slug}/manage/save", of("author", "slug"), of());
+        register("projects.showNotes", "/{author}/{slug}/notes", of("author", "slug"), of());
+        register("projects.setVisible", "/{author}/{slug}/visible/{visibility}", of("author", "slug", "visibility"), of());
+        register("projects.removeMember", "/{author}/{slug}/manage/members/remove", of("author", "slug"), of());
+
+        register("versions.restore", "/{author}/{slug}/versions/{version}/restore", of("author", "slug", "version"), of());
+        register("versions.downloadRecommendedJar", "/{author}/{slug}/versions/recommended/jar", of("author", "slug"), of("token"));
+        register("versions.publish", "/{author}/{slug}/versions/{version}", of("author", "slug", "version"), of());
+        register("versions.setRecommended", "/{author}/{slug}/versions/{version}/recommended", of("author", "slug", "version"), of());
+        register("versions.download", "/{author}/{slug}/versions/{version}/download", of("author", "slug", "version"), of("token", "confirm"));
+        register("versions.showLog", "/{author}/{slug}/versionLog", of("author", "slug"), of("versionString"));
+        register("versions.show", "/{author}/{slug}/versions/{version}", of("author", "slug", "version"), of());
+        register("versions.downloadJar", "/{author}/{slug}/versions/{version}/jar", of("author", "slug", "version"), of("token"));
+        register("versions.approve", "/{author}/{slug}/versions/{version}/approve", of("author", "slug", "version"), of());
+        register("versions.approvePartial", "/{author}/{slug}/versions/{version}/approvePartial", of("author", "slug", "version"), of());
+        register("versions.saveDescription", "/{author}/{slug}/versions/{version}/save", of("author", "slug", "version"), of());
+        register("versions.downloadRecommended", "/{author}/{slug}/versions/recommended/download", of("author", "slug"), of("token"));
+        register("versions.showList", "/{author}/{slug}/versions", of("author", "slug"), of());
+        register("versions.downloadJarById", "/api/project/{pluginId}/versions/{name}/download", of("pluginId", "name"), of("token"));
+        register("versions.downloadRecommendedJarById", "/api/project/{pluginId}/versions/recommended/download", of("pluginId"), of("token"));
+        register("versions.upload", "/{author}/{slug}/versions/new/upload", of("author", "slug"), of());
+        register("versions.softDelete", "/{author}/{slug}/versions/{version}/delete", of("author", "slug", "version"), of());
+        register("versions.showDownloadConfirm", "/{author}/{slug}/versions/{version}/confirm", of("author", "slug", "version"), of("downloadType", "api", "dummy"));
+        register("versions.showCreator", "/{author}/{slug}/versions/new", of("author", "slug"), of());
+        register("versions.delete", "/{author}/{slug}/versions/{version}/hardDelete", of("author", "slug", "version"), of());
+        register("versions.showCreatorWithMeta", "/{author}/{slug}/versions/new/{version}", of("author", "slug", "version"), of());
+        register("versions.confirmDownload", "/{author}/{slug}/versions/{version}/confirm", of("author", "slug", "version"), of("downloadType", "api", "dummy"));
+
+        register("pages.showPreview", "/pages/preview", of(), of());
+        register("pages.save", "/{author}/{slug}/pages/{page}/edit", of("author", "slug", "page"), of());
+        register("pages.showEditor", "/{author}/{slug}/pages/{page}/edit", of("author", "slug", "page"), of());
+        register("pages.show", "/{author}/{slug}/pages/{page}", of("author", "slug", "page"), of());
+        register("pages.delete", "/{author}/{slug}/pages/{page}/delete", of("author", "slug", "page"), of());
 
         register("users.showAuthors", "/authors", of(), of("sort", "page"));
         register("users.saveTagline", "/{user}/settings/tagline", of("user"), of());
@@ -73,6 +125,11 @@ public class RouteHelper {
         register("reviews.takeoverReview", "/{author}/{slug}/versions/{version}/reviews/takeover", of("author", "slug", "version"), of());
         register("reviews.reopenReview", "/{author}/{slug}/versions/{version}/reviews/reopen", of("author", "slug", "version"), of());
 
+        register("channels.delete", "/{author}/{slug}/channels/{channel}/delete", of("author", "slug", "channel"), of());
+        register("channels.save", "/{author}/{slug}/channels/{channel}", of("author", "slug", "channel"), of());
+        register("channels.showList", "/{author}/{slug}/channels", of("author", "slug"), of());
+        register("channels.create", "/{author}/{slug}/channels", of("author", "slug"), of());
+
         register("apiv1.showVersion", "/api/v1/projects/{pluginId}/versions/{name}", of("pluginId", "name"), of());
         register("apiv1.listProjects", "/api/v1/projects", of(), of("categories", "sort", "q", "limit", "offset"));
         register("apiv1.listUsers", "/api/v1/users", of(), of("limit", "offset"));
@@ -88,13 +145,6 @@ public class RouteHelper {
         register("apiv1.deployVersion", "/api/v1/projects/{pluginId}/versions/{name}", of("pluginId", "name"), of());
         register("apiv1.listTags", "/api/v1/projects/{plugin}/tags/{versionName}", of("plugin", "versionName"), of());
 
-        //TODO
-        /*
-        GET     /:author/:slug/pages/*page/edit                             @controllers.project.Pages.showEditor(author, slug, page)
-        POST    /:author/:slug/pages/*page/edit                             @controllers.project.Pages.save(author, slug, page)
-        POST    /:author/:slug/pages/*page/delete                           @controllers.project.Pages.delete(author, slug, page)
-        GET     /:author/:slug/pages/*page                                  @controllers.project.Pages.show(author, slug, page)
-         */
     }
 
     // TODO support route params
