773224a56d891b2e54e93f34f86179e11f65aefa
Jake Potrebic <jake.m.potrebic@gmail.com>
2020-07-28 23:21:04 -0700

    done for night

--- src/main/java/me/minidigger/hangar/service/project/ProjectService.java
+++ src/main/java/me/minidigger/hangar/service/project/ProjectService.java
@@ -26,10 +26,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-<<<<<<<HEAD
-=======
-        >>>>>>>partial work on pages
-
 @Service
 public class ProjectService {
 
