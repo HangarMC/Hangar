d24e749a882da8c1a196b934a31c030d09defff9
md678685 <1917406+md678685@users.noreply.github.com>
2020-08-01 18:09:57 +0100

    Use proper logger not just System.out

--- src/main/java/me/minidigger/hangar/controller/api/SsoApiController.java
+++ src/main/java/me/minidigger/hangar/controller/api/SsoApiController.java
@@ -2,8 +2,11 @@ package me.minidigger.hangar.controller.api;
 
 import me.minidigger.hangar.config.HangarConfig;
 import me.minidigger.hangar.model.generated.SsoSyncData;
+import me.minidigger.hangar.service.RoleService;
 import me.minidigger.hangar.service.UserService;
 import me.minidigger.hangar.service.SsoService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -19,6 +22,8 @@ public class SsoApiController implements SsoApi {
     private final SsoService signer;
     private final HangarConfig.HangarSsoConfig ssoConfig;
 
+    private static final Logger log = LoggerFactory.getLogger(SsoApiController.class);
+
     @Autowired
     public SsoApiController(UserService userService, SsoService signer, HangarConfig.HangarSsoConfig ssoConfig) {
         this.userService = userService;
@@ -29,7 +34,7 @@ public class SsoApiController implements SsoApi {
     @Override
     public ResponseEntity<Void> syncSso(@NotEmpty String sso, @NotEmpty String sig, @RequestParam(name = "api_key") @NotEmpty String apiKey) {
         if (!apiKey.equals(ssoConfig.getApiKey())) {
-            System.out.println("FAILED SSO SYNC: bad API key (provided " + apiKey + " not " + ssoConfig.getApiKey() + ")");
+            log.warn("SSO sync failed: bad API key (" + apiKey + " provided, " + ssoConfig.getApiKey() + " expected)");
             return ResponseEntity.badRequest().build();
         }
 
@@ -37,10 +42,10 @@ public class SsoApiController implements SsoApi {
             Map<String, String> map = signer.decode(sso, sig);
             SsoSyncData data = SsoSyncData.fromSignedPayload(map);
             userService.ssoSyncUser(data);
-            System.out.println("SUCCESSFUL SSO SYNC: " + map.toString());
+            log.debug("SSO sync successful: " + map.toString());
             return ResponseEntity.ok().build();
         } catch (SsoService.SignatureException e) {
-            System.out.println("FAILED SSO SYNC: invalid signature (" + sig + " for data " + sso + ")");
+            log.warn("SSO sync failed: invalid signature (" + sig + " for data " + sso + ")");
             return ResponseEntity.badRequest().build();
         }
     }
