aa46c04aaab9cf6d594092523fc4533824065342
MiniDigger <admin@minidigger.me>
2020-08-03 22:32:08 +0200

    more caching + disable dum test

--- src/test/java/me/minidigger/hangar/HangarApplicationTests.java
+++ src/test/java/me/minidigger/hangar/HangarApplicationTests.java
@@ -1,8 +1,10 @@
 package me.minidigger.hangar;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@Disabled
 @SpringBootTest
 class HangarApplicationTests {
 
