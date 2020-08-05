be55b26968e9e889386b460bb009950439bbfd68
KennyTV <kennytv@t-online.de>
2020-08-05 10:16:20 +0200

    Remove some leftovers

--- src/main/java/me/minidigger/hangar/controller/ChannelsController.java
+++ src/main/java/me/minidigger/hangar/controller/ChannelsController.java
@@ -5,11 +5,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.ModelAndView;
-
-import me.minidigger.hangar.controller.HangarController;
 
 @Controller
 public class ChannelsController extends HangarController {
