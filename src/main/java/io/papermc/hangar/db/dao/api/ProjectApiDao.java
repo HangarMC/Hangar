ddbd1a4d919797545945bc7cdcd5b8506e4a9d55
KennyTV <jahnke.nassim@gmail.com>
2020-08-29 11:42:22 +0200

    Fix Mini's rebase, minor cleanup ( ͡ಠ ʖ̯ ͡ಠ)

--- src/main/java/io/papermc/hangar/db/dao/api/ProjectApiDao.java
+++ src/main/java/io/papermc/hangar/db/dao/api/ProjectApiDao.java
@@ -1,7 +1,11 @@
 package io.papermc.hangar.db.dao.api;
 
+import io.papermc.hangar.db.mappers.PromotedVersionMapper;
+import io.papermc.hangar.model.Category;
+import io.papermc.hangar.model.generated.Project;
 import org.jdbi.v3.core.enums.EnumByOrdinal;
 import org.jdbi.v3.sqlobject.config.RegisterBeanMapper;
+import org.jdbi.v3.sqlobject.config.RegisterColumnMapper;
 import org.jdbi.v3.sqlobject.customizer.BindList;
 import org.jdbi.v3.sqlobject.customizer.Define;
 import org.jdbi.v3.sqlobject.customizer.DefineNamedBindings;
@@ -11,54 +15,52 @@ import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-import io.papermc.hangar.model.Category;
-import io.papermc.hangar.model.generated.Project;
-
 @Repository
 @RegisterBeanMapper(Project.class)
 public interface ProjectApiDao {
 
     @UseStringTemplateEngine
     @SqlQuery("SELECT p.created_at," +
-                "       p.plugin_id," +
-                        "       p.name," +
-                        "       p.owner_name," +
-                        "       p.slug," +
-                        "       p.promoted_versions," +
-                        "       p.views," +
-                        "       p.downloads," +
-                        "       p.recent_views," +
-                        "       p.recent_downloads," +
-                        "       p.stars," +
-                        "       p.watchers," +
-                        "       p.category," +
-                        "       p.description," +
-                        "       COALESCE(p.last_updated, p.created_at) AS last_updated," +
-                        "       p.visibility, " +
-                        "       <if(requesterId)> " +
-                        "         EXISTS(SELECT * FROM project_stars s WHERE s.project_id = p.id AND s.user_id = :requesterId) AS user_stared, " +
-                        "         EXISTS(SELECT * FROM project_watchers s WHERE s.project_id = p.id AND s.user_id = :requesterId) AS user_watching, " +
-                        "       <endif>" +
-                        "       ps.homepage," +
-                        "       ps.issues," +
-                        "       ps.source," +
-                        "       ps.support," +
-                        "       ps.license_name," +
-                        "       ps.license_url," +
-                        "       ps.forum_sync" +
-                        "  FROM home_projects p" +
-                        "         JOIN projects ps ON p.id = ps.id" +
-                        "         WHERE true " + //Not sure how else to get here a single Where
-                        "         <if(pluginId)> AND (p.plugin_id = :pluginId) <endif> " +
-                        "         <if(owner)> AND (p.owner_name = :owner) <endif> " +
-                        "         <if(!seeHidden)> AND (p.visibility = 1 OR (:requesterId = ANY(p.project_members) AND p.visibility != 5)) <endif> " +
-                        "         <if(categories)> AND (p.category in (<categories>)) <endif> " +
-                        "         <if(query)> AND ( <queryStatement> ) <endif> " +
-                        "         <if(tags)> AND EXISTS ( SELECT pv.tag_name FROM jsonb_to_recordset(p.promoted_versions) " +
-                        "           AS pv(tag_name TEXT, tag_version TEXT) WHERE (pv.tag_name) in (<tags>) ) <endif> " +
-                        "         ORDER BY <orderBy> " +
-                        "         LIMIT :limit" +
-                        "         OFFSET :offset")
+            "       p.plugin_id," +
+            "       p.name," +
+            "       p.owner_name," +
+            "       p.slug," +
+            "       p.promoted_versions," +
+            "       p.views," +
+            "       p.downloads," +
+            "       p.recent_views," +
+            "       p.recent_downloads," +
+            "       p.stars," +
+            "       p.watchers," +
+            "       p.category," +
+            "       p.description," +
+            "       COALESCE(p.last_updated, p.created_at) AS last_updated," +
+            "       p.visibility, " +
+            "       <if(requesterId)> " +
+            "         EXISTS(SELECT * FROM project_stars s WHERE s.project_id = p.id AND s.user_id = :requesterId) AS user_stared, " +
+            "         EXISTS(SELECT * FROM project_watchers s WHERE s.project_id = p.id AND s.user_id = :requesterId) AS user_watching, " +
+            "       <endif>" +
+            "       ps.homepage," +
+            "       ps.issues," +
+            "       ps.source," +
+            "       ps.support," +
+            "       ps.license_name," +
+            "       ps.license_url," +
+            "       ps.forum_sync" +
+            "  FROM home_projects p" +
+            "         JOIN projects ps ON p.id = ps.id" +
+            "         WHERE true " + //Not sure how else to get here a single Where
+            "         <if(pluginId)> AND (p.plugin_id = :pluginId) <endif> " +
+            "         <if(owner)> AND (p.owner_name = :owner) <endif> " +
+            "         <if(!seeHidden)> AND (p.visibility = 1 OR (:requesterId = ANY(p.project_members) AND p.visibility != 5)) <endif> " +
+            "         <if(categories)> AND (p.category in (<categories>)) <endif> " +
+            "         <if(query)> AND ( <queryStatement> ) <endif> " +
+            "         <if(tags)> AND EXISTS ( SELECT pv.tag_name FROM jsonb_to_recordset(p.promoted_versions) " +
+            "           AS pv(tag_name TEXT, tag_version TEXT) WHERE (pv.tag_name) in (<tags>) ) <endif> " +
+            "         ORDER BY <orderBy> " +
+            "         LIMIT :limit" +
+            "         OFFSET :offset")
+    @RegisterColumnMapper(PromotedVersionMapper.class)
     @DefineNamedBindings
     List<Project> listProjects(String pluginId, String owner, boolean seeHidden, Long requesterId, @Define String orderBy,
                                @BindList(onEmpty = BindList.EmptyHandling.NULL_VALUE) @EnumByOrdinal List<Category> categories,
@@ -67,43 +69,43 @@ public interface ProjectApiDao {
 
     @UseStringTemplateEngine
     @SqlQuery("SELECT COUNT(*) FROM ( SELECT p.created_at," +
-              "       p.plugin_id," +
-              "       p.name," +
-              "       p.owner_name," +
-              "       p.slug," +
-              "       p.promoted_versions," +
-              "       p.views," +
-              "       p.downloads," +
-              "       p.recent_views," +
-              "       p.recent_downloads," +
-              "       p.stars," +
-              "       p.watchers," +
-              "       p.category," +
-              "       p.description," +
-              "       COALESCE(p.last_updated, p.created_at) AS last_updated," +
-              "       p.visibility, " +
-              "       <if(requesterId)> " +
-              "         EXISTS(SELECT * FROM project_stars s WHERE s.project_id = p.id AND s.user_id = :requesterId) AS user_stared, " +
-              "         EXISTS(SELECT * FROM project_watchers s WHERE s.project_id = p.id AND s.user_id = :requesterId) AS user_watching, " +
-              "       <endif> "+
-              "       ps.homepage," +
-              "       ps.issues," +
-              "       ps.source," +
-              "       ps.support," +
-              "       ps.license_name," +
-              "       ps.license_url," +
-              "       ps.forum_sync" +
-              "  FROM home_projects p" +
-              "         JOIN projects ps ON p.id = ps.id" +
-              "         WHERE true " + //Not sure how else to get here a single Where
-              "         <if(pluginId)> AND (p.plugin_id = :pluginId) <endif> " +
-              "         <if(owner)> AND (p.owner_name = :owner) <endif> " +
-              "         <if(!seeHidden)> AND (p.visibility = 1 OR (:requesterId = ANY(p.project_members) AND p.visibility != 5)) <endif> " +
-              "         <if(categories)> AND (p.category in (<categories>)) <endif> " +
-              "         <if(query)> AND ( <queryStatement> ) <endif> " +
-              "         <if(tags)> AND EXISTS ( SELECT pv.tag_name FROM jsonb_to_recordset(p.promoted_versions) " +
-              "           AS pv(tag_name TEXT, tag_version TEXT) WHERE (pv.tag_name) in (<tags>) ) <endif> " +
-              "         ) sq")
+            "       p.plugin_id," +
+            "       p.name," +
+            "       p.owner_name," +
+            "       p.slug," +
+            "       p.promoted_versions," +
+            "       p.views," +
+            "       p.downloads," +
+            "       p.recent_views," +
+            "       p.recent_downloads," +
+            "       p.stars," +
+            "       p.watchers," +
+            "       p.category," +
+            "       p.description," +
+            "       COALESCE(p.last_updated, p.created_at) AS last_updated," +
+            "       p.visibility, " +
+            "       <if(requesterId)> " +
+            "         EXISTS(SELECT * FROM project_stars s WHERE s.project_id = p.id AND s.user_id = :requesterId) AS user_stared, " +
+            "         EXISTS(SELECT * FROM project_watchers s WHERE s.project_id = p.id AND s.user_id = :requesterId) AS user_watching, " +
+            "       <endif> " +
+            "       ps.homepage," +
+            "       ps.issues," +
+            "       ps.source," +
+            "       ps.support," +
+            "       ps.license_name," +
+            "       ps.license_url," +
+            "       ps.forum_sync" +
+            "  FROM home_projects p" +
+            "         JOIN projects ps ON p.id = ps.id" +
+            "         WHERE true " + //Not sure how else to get here a single Where
+            "         <if(pluginId)> AND (p.plugin_id = :pluginId) <endif> " +
+            "         <if(owner)> AND (p.owner_name = :owner) <endif> " +
+            "         <if(!seeHidden)> AND (p.visibility = 1 OR (:requesterId = ANY(p.project_members) AND p.visibility != 5)) <endif> " +
+            "         <if(categories)> AND (p.category in (<categories>)) <endif> " +
+            "         <if(query)> AND ( <queryStatement> ) <endif> " +
+            "         <if(tags)> AND EXISTS ( SELECT pv.tag_name FROM jsonb_to_recordset(p.promoted_versions) " +
+            "           AS pv(tag_name TEXT, tag_version TEXT) WHERE (pv.tag_name) in (<tags>) ) <endif> " +
+            "         ) sq")
     @DefineNamedBindings
     long countProjects(String pluginId, String owner, @Define boolean seeHidden, Long requesterId,
                        @BindList(onEmpty = BindList.EmptyHandling.NULL_VALUE) @EnumByOrdinal List<Category> categories,
--- src/main/java/io/papermc/hangar/db/mappers/PromotedVersionMapper.java
+++ src/main/java/io/papermc/hangar/db/mappers/PromotedVersionMapper.java
@@ -8,7 +8,6 @@ import io.papermc.hangar.db.customtypes.JSONB;
 import io.papermc.hangar.model.generated.PromotedVersion;
 import org.jdbi.v3.core.mapper.ColumnMapper;
 import org.jdbi.v3.core.statement.StatementContext;
-import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.sql.ResultSet;
@@ -17,12 +16,15 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class PromotedVersionMapper implements ColumnMapper<List<PromotedVersion>> {
-    public List<PromotedVersion> map(ResultSet r, int columnNumber, StatementContext ctx) throws SQLException {
-        JSONB jsonb = (JSONB) r.getObject(columnNumber);
-//        LoggerFactory.getLogger(PromotedVersionMapper.class).info(jsonb.toString()); // for json logging
+
+    @Override
+    public List<PromotedVersion> map(ResultSet rs, int column, StatementContext ctx) throws SQLException {
+        JSONB jsonb = (JSONB) rs.getObject(column);
         JsonNode jsonNode = jsonb.getJson();
         ObjectMapper mapper = new ObjectMapper();
-        ObjectReader reader = mapper.readerFor(new TypeReference<List<PromotedVersion>>() {});
+        ObjectReader reader = mapper.readerFor(new TypeReference<List<PromotedVersion>>() {
+        });
+
         List<PromotedVersion> list = new ArrayList<>();
         try {
             list = reader.readValue(jsonNode);
