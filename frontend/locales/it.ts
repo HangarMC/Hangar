e4b44b32717712caf062e35ccb2fb7d1cb74caf0
MiniDigger | Martin <admin@benndorf.dev>
2021-12-17 15:46:56 +0100

    New translations en.ts (Italian)

--- frontend/locales/it.ts
+++ frontend/locales/it.ts
@@ -80,7 +80,8 @@ const msgs: LocaleMessageObject = {
       javadocs: 'JavaDocs',
       hangar: 'Hangar (Plugins)',
       downloads: "Download",
-      community: 'Community'
+      community: 'Community',
+      auth: 'Authentication Portal'
     }
   },
   project: {
