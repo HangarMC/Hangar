c74068ab56850e197566cd762a72490ef1a9f0de
Alessio Gravili <70709113+AlessioGr@users.noreply.github.com>
2022-01-29 15:29:50 +0100

    Improve Create new button text (#588)
    Co-authored-by: MiniDigger | Martin <admin@benndorf.dev>

--- frontend/locales/en.ts
+++ frontend/locales/en.ts
@@ -72,7 +72,7 @@ export default {
         fakeUserEnabled: 'Fake user is enabled. {0} is therefore disabled'
       }
     },
-    createNew: 'Create new...',
+    createNew: 'New',
     new: {
       project: 'New Project',
       organization: 'New Organization'
