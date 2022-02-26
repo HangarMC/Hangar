760325a1f89d415a053293dff367b536580e849b
Alessio Gravili <alessio@gravili.de>
2022-02-26 19:09:10 +0100

    Search bar

--- frontend-new/src/pages/index.vue
+++ frontend-new/src/pages/index.vue
@@ -1,12 +1,26 @@
 <script setup lang="ts">
 const { t } = useI18n();
+
+
 </script>
 
 <template>
-  <div>Index Page {{ t("general.close") }}</div>
+  <div class="flex flex-col items-center pt-10">
+      <h2 class="text-3xl font-bold uppercase">Search your favorite plugins</h2>
+      <input class="mt-6 rounded-md big-box-shadow p-3 w-4/5 max-w-800px focus-visible:(border-white)" type="text" placeholder="Search in 1 projects, proudly made by the community...">
+  </div>
 </template>
 
 <route lang="yaml">
 meta:
   layout: default
 </route>
+
+<style lang="css" scoped>
+
+.big-box-shadow {
+    box-shadow: 0 0 10px 0 #004ee99e;
+}
+
+
+</style>
