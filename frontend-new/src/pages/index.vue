26a087172e3980670bd44834f6cee8542372cf20
Alessio Gravili <alessio@gravili.de>
2022-02-26 20:46:12 +0100

    Scrollbar & dark mode input

--- frontend-new/src/pages/index.vue
+++ frontend-new/src/pages/index.vue
@@ -7,7 +7,7 @@ const { t } = useI18n();
 <template>
   <div class="flex flex-col items-center pt-10">
       <h2 class="text-3xl font-bold uppercase">Search your favorite plugins</h2>
-      <input class="mt-6 rounded-md big-box-shadow p-3 w-4/5 max-w-800px focus-visible:(border-white)" type="text" placeholder="Search in 1 projects, proudly made by the community...">
+      <input class="mt-6 rounded-md big-box-shadow p-3 w-4/5 max-w-800px focus-visible:(border-white) text-black" type="text" placeholder="Search in 1 projects, proudly made by the community...">
   </div>
 </template>
 
