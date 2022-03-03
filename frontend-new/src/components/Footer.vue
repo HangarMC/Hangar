799e7e2dcce51742d84b7dafffe4e89310685199
Alessio Gravili <70709113+AlessioGr@users.noreply.github.com>
2022-03-03 02:22:27 +0100

    Improve style (#607)

--- frontend-new/src/components/Footer.vue
+++ frontend-new/src/components/Footer.vue
@@ -1,40 +1,42 @@
 <script setup lang="ts"></script>
 
 <template>
-  <footer class="relative flex items-end mt-10 bg-gradient-to-r from-[#004ee9] to-[#367aff] px-8 pt-20 pb-2 text-[#f8faff] min-h-70">
+  <footer class="relative flex items-end mt-10 bg-gradient-to-r from-[#004ee9] to-[#367aff] px-8 pt-15 pb-2 text-background-light-10 min-h-70">
     <div class="footerContent w-screen">
-      <div class="flex justify-center flex-col md:flex-row justify-center gap-y-6 md:gap-y-0 md:gap-x-6 max-w-1200px m-auto">
-        <div class="md:(w-1/3 min-w-1/3 max-w-1/3)">
-          <p class="text-xl font-bold text-center mb-2">About Hangar</p>
-          <p>
+      <div class="flex justify-center flex-col justify-center gap-y-6 max-w-1200px m-auto" md="gap-x-6 gap-y-0 flex-row">
+        <div md="w-1/3 min-w-1/3 max-w-1/3">
+          <p class="text-l font-bold text-center mb-2">About Hangar</p>
+          <p class="text-sm">
             Hangar is an NFT plugin repository, hosted in the blockchain. Every plugin is unique and owned just by you. Can you use it on your server? No! You
-            only own the receipt. And that's what makes Hangar so eco-friendly.
+            only own the receipt.
           </p>
         </div>
-        <div class="md:(w-1/3 min-w-1/3 max-w-1/3)">
-          <p class="text-xl font-bold text-center mb-2">Links</p>
+        <div md="w-1/3 min-w-1/3 max-w-1/3">
+          <p class="text-l font-bold text-center mb-2">Links</p>
           <div class="flex flex-col items-center gap-y-3">
             <router-link
               :to="{ name: 'staff' }"
-              class="flex items-center justify-center rounded-md px-4 py-2 light:text-black background-body w-fit"
+              class="flex items-center justify-center rounded-md px-4 py-2 background-body w-fit text-sm"
+              light="text-black"
               hover="text-primary-100"
             >
               Open Crypto Wallet
             </router-link>
             <router-link
               :to="{ name: 'staff' }"
-              class="flex items-center justify-center rounded-md px-4 py-2 light:text-black background-body w-fit"
+              class="flex items-center justify-center rounded-md px-4 py-2 background-body w-fit text-sm"
+              light="text-black"
               hover="text-primary-100"
             >
               Exchange Plugin NFTs
             </router-link>
           </div>
         </div>
-        <div class="md:(w-1/3 min-w-1/3 max-w-1/3)">
-          <p class="text-xl font-bold text-center mb-2">More from Paper</p>
+        <div md="w-1/3 min-w-1/3 max-w-1/3">
+          <p class="text-l font-bold text-center mb-2">More from Paper</p>
         </div>
       </div>
-      <div class="footerBar flex items-center justify-around mt-4 max-w-1200px m-auto">
+      <div class="footerBar flex items-center justify-around mt-4 max-w-1200px m-auto text-sm">
         <p>Copyright © <a href="https://papermc.io/">PaperMC</a> 2016 - 2022</p>
         <div class="footerBarLinks flex">
           <router-link :to="{ name: 'staff' }" class="flex items-center rounded-md px-6 py-2" hover="text-primary-100 bg-primary-50">About Hangar </router-link>
@@ -49,7 +51,8 @@
       <!--- <svg class="fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 218" preserveAspectRatio="none"><path d="M0 218h1200v-31.3l-40 4.4c-40 4.8-120 13.1-200 0-80-13.6-160-48.6-240-66.7-80-17.8-160-17.8-240-8.8-80 8.6-160 26.9-240 8.8-80-17.7-160-71.1-200-97.7L0 0v218z"></path></svg>
         -->
       <svg
-        class="fill-background-light-10 dark:fill-background-dark-80 h-240px min-w-full"
+        class="fill-background-light-10 h-240px min-w-full"
+        dark="fill-background-dark-80"
         xmlns="http://www.w3.org/2000/svg"
         viewBox="0 0 1200 218"
         preserveAspectRatio="none"
--- frontend-new/src/components/Header.vue
+++ frontend-new/src/components/Header.vue
@@ -1,12 +1,24 @@
 <script setup lang="ts">
-import { Popover, PopoverButton, PopoverPanel } from "@headlessui/vue";
 import type { Announcement as AnnouncementObject } from "hangar-api";
+import { Popover, PopoverButton, PopoverPanel } from "@headlessui/vue";
 import type { Ref } from "vue";
-import { ref } from "vue";
 import { useI18n } from "vue-i18n";
-import Announcement from "~/components/Announcement.vue";
-import { useAPI } from "~/store/api";
+import { ref } from "vue";
 import { useThemeStore } from "~/store/theme";
+import { useAPI } from "~/store/api";
+import Announcement from "~/components/Announcement.vue";
+
+import hangarLogo from "/logo.svg";
+
+import IconMdiHome from "~icons/mdi/home";
+import IconMdiAccountGroup from "~icons/mdi/account-group";
+import IconMdiForum from "~icons/mdi/forum";
+import IconMdiCodeBraces from "~icons/mdi/code-braces";
+import IconMdiBookOpen from "~icons/mdi/book-open";
+import IconMdiLanguageJava from "~icons/mdi/language-java";
+import IconMdiPuzzle from "~icons/mdi/puzzle";
+import IconMdiDownloadCircle from "~icons/mdi/download-circle";
+import IconMdiKey from "~icons/mdi/key";
 
 const theme = useThemeStore();
 const { t } = useI18n();
@@ -36,6 +48,23 @@ const navBarLinks = [
   { link: "staff", label: "Team" },
 ];
 
+const navBarMenuLinksHangar = [
+  { link: "index", label: "Home", icon: IconMdiHome },
+  { link: "staff", label: "Team", icon: IconMdiAccountGroup },
+];
+
+const navBarMenuLinksMoreFromPaper = [
+  { link: "https://papermc.io/", label: t("nav.hangar.home"), icon: IconMdiHome },
+  { link: "https://forums.papermc.io/", label: t("nav.hangar.forums"), icon: IconMdiForum },
+  { link: "https://github.com/PaperMC", label: t("nav.hangar.code"), icon: IconMdiCodeBraces },
+  { link: "https://paper.readthedocs.io/en/latest/", label: t("nav.hangar.docs"), icon: IconMdiBookOpen },
+  { link: "https://papermc.io/javadocs", label: t("nav.hangar.javadocs"), icon: IconMdiLanguageJava },
+  { link: "/", label: t("nav.hangar.hangar"), icon: IconMdiPuzzle },
+  { link: "https://papermc.io/downloads", label: t("nav.hangar.downloads"), icon: IconMdiDownloadCircle },
+  { link: "https://papermc.io/community", label: t("nav.hangar.community"), icon: IconMdiAccountGroup },
+  { link: "https://hangar-auth.benndorf.dev/", label: t("nav.hangar.auth"), icon: IconMdiKey },
+];
+
 const loggedIn = false; // TODO
 </script>
 
@@ -59,57 +88,34 @@ const loggedIn = false; // TODO
             leave-to-class="translate-y-1 opacity-0"
           >
             <PopoverPanel
-              class="fixed md:absolute z-10 w-9/10 md:w-max background-header top-1/14 md:top-10 left-1/20 shadow1 rounded-md md:rounded-none md:rounded-bl-md md:rounded-r-md border-top-primary text-xs p-[20px]"
+              class="fixed z-10 w-9/10 background-header top-1/14 left-1/20 shadow1 rounded-md border-top-primary text-xs p-[20px]"
+              md="absolute w-max top-10 rounded-none rounded-bl-md rounded-r-md"
             >
               <p class="text-base font-semibold color-primary mb-4">Hangar</p>
               <div class="grid grid-cols-2">
-                <router-link :to="{ name: 'index' }" class="flex items-center rounded-md px-6 py-2" hover="text-primary-100 bg-primary-50">
-                  <icon-mdi-home class="mr-3 text-[1.2em]" />
-                  Home
-                </router-link>
-                <router-link :to="{ name: 'staff' }" class="flex items-center rounded-md px-6 py-2" hover="text-primary-100 bg-primary-50">
-                  <icon-mdi-account-group class="mr-3 text-[1.2em]" />
-                  Team
+                <router-link
+                  v-for="link in navBarMenuLinksHangar"
+                  :key="link.label"
+                  :to="{ name: link.link }"
+                  class="flex items-center rounded-md px-6 py-2"
+                  hover="text-primary-100 bg-primary-50"
+                >
+                  <component :is="link.icon" class="mr-3 text-[1.2em]" />
+                  {{ link.label }}
                 </router-link>
               </div>
 
               <p class="text-base font-semibold color-primary mb-4 mt-10">More from Paper</p>
               <div class="grid grid-cols-2">
-                <a class="flex items-center rounded-md px-6 py-2" href="https://papermc.io/" hover="text-primary-100 bg-primary-50">
-                  <icon-mdi-home class="mr-3 text-[1.2em]" />
-                  {{ t("nav.hangar.home") }}
-                </a>
-                <a class="flex items-center rounded-md px-6 py-2" href="https://forums.papermc.io/" hover="text-primary-100 bg-primary-50">
-                  <icon-mdi-forum class="mr-3 text-[1.2em]" />
-                  {{ t("nav.hangar.forums") }}
-                </a>
-                <a class="flex items-center rounded-md px-6 py-2" href="https://github.com/PaperMC" hover="text-primary-100 bg-primary-50">
-                  <icon-mdi-code-braces class="mr-3 text-[1.2em]" />
-                  {{ t("nav.hangar.code") }}
-                </a>
-                <a class="flex items-center rounded-md px-6 py-2" href="https://paper.readthedocs.io/en/latest/" hover="text-primary-100 bg-primary-50">
-                  <icon-mdi-book-open class="mr-3 text-[1.2em]" />
-                  {{ t("nav.hangar.docs") }}
-                </a>
-                <a class="flex items-center rounded-md px-6 py-2" href="https://papermc.io/javadocs" hover="text-primary-100 bg-primary-50">
-                  <icon-mdi-language-java class="mr-3 text-[1.2em]" />
-                  {{ t("nav.hangar.javadocs") }}
-                </a>
-                <a class="flex items-center rounded-md px-6 py-2" href="/" hover="text-primary-100 bg-primary-50">
-                  <icon-mdi-puzzle class="mr-3 text-[1.2em]" />
-                  {{ t("nav.hangar.hangar") }}
-                </a>
-                <a class="flex items-center rounded-md px-6 py-2" href="https://papermc.io/downloads" hover="text-primary-100 bg-primary-50">
-                  <icon-mdi-download-circle class="mr-3 text-[1.2em]" />
-                  {{ t("nav.hangar.downloads") }}
-                </a>
-                <a class="flex items-center rounded-md px-6 py-2" href="https://papermc.io/community" hover="text-primary-100 bg-primary-50">
-                  <icon-mdi-account-group class="mr-3 text-[1.2em]" />
-                  {{ t("nav.hangar.community") }}
-                </a>
-                <a class="flex items-center rounded-md px-6 py-2" href="https://hangar-auth.benndorf.dev/" hover="text-primary-100 bg-primary-50">
-                  <icon-mdi-key class="mr-3 text-[1.2em]" />
-                  {{ t("nav.hangar.auth") }}
+                <a
+                  v-for="link in navBarMenuLinksMoreFromPaper"
+                  :key="link.label"
+                  class="flex items-center rounded-md px-6 py-2"
+                  :href="link.link"
+                  hover="text-primary-100 bg-primary-50"
+                >
+                  <component :is="link.icon" class="mr-3 text-[1.2em]" />
+                  {{ link.label }}
                 </a>
               </div>
             </PopoverPanel>
@@ -117,14 +123,15 @@ const loggedIn = false; // TODO
         </Popover>
 
         <div class="site-logo mr-4 h-60px flex items-center">
-          <img alt="Hangar Logo" src="/logo.svg" class="h-50px object-cover" />
+          <img alt="Hangar Logo" :src="hangarLogo" class="h-50px object-cover" />
         </div>
         <nav class="flex gap-3 invisible md:visible">
           <router-link
             v-for="navBarLink in navBarLinks"
             :key="navBarLink.label"
             :to="{ name: navBarLink.link }"
-            class="relative after:(absolute content-DEFAULT block w-0 top-30px left-1/10 h-4px rounded-8px)"
+            class="relative"
+            after="absolute content-DEFAULT block w-0 top-30px left-1/10 h-4px rounded-8px"
           >
             {{ navBarLink.label }}
           </router-link>
