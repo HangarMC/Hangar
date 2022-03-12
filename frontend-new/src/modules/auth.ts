e36f913fb0867fcf8f4ef006b58d289b09b4e071
MiniDigger <admin@benndorf.dev>
2022-03-12 10:47:58 +0100

    fix login hydration
    Signed-off-by: MiniDigger <admin@benndorf.dev>

--- frontend-new/src/modules/auth.ts
+++ frontend-new/src/modules/auth.ts
@@ -2,6 +2,8 @@ import type { UserModule } from "~/types";
 import { useAuth } from "~/composables/useAuth";
 import { useCookies } from "~/composables/useCookies";
 import { set, unset } from "~/composables/useResReq";
+import { authLog } from "~/composables/useLog";
+import { useAuthStore } from "~/store/auth";
 
 export const install: UserModule = async ({ request, response, router }) => {
   router.beforeEach(async (to) => {
@@ -11,6 +13,10 @@ export const install: UserModule = async ({ request, response, router }) => {
     }
     set(request, response);
     if (useCookies().get("HangarAuth_REFRESH")) {
+      authLog("Got refresh cookie, calling refresh...");
+      await useAuth.refreshUser();
+    } else if (useAuthStore().authenticated) {
+      authLog("Logged in on the backend, calling refresh...");
       await useAuth.refreshUser();
     }
     // TODO load route permissions here
--- frontend-new/src/store/auth.ts
+++ frontend-new/src/store/auth.ts
@@ -1,6 +1,7 @@
 import type { HangarUser } from "hangar-internal";
 import { defineStore } from "pinia";
 import { ref } from "vue";
+import { authLog } from "~/composables/useLog";
 
 export const useAuthStore = defineStore("auth", () => {
   const authenticated = ref<boolean>(false);
@@ -8,5 +9,11 @@ export const useAuthStore = defineStore("auth", () => {
   const token = ref<string | null>(null);
   const routePermissions = ref<string | null>(null);
 
-  return { authenticated, user, token, routePermissions };
+  authLog("create authStore");
+
+  function setUser(newUser: HangarUser) {
+    user.value = newUser;
+  }
+
+  return { authenticated, user, token, routePermissions, setUser };
 });
