c9d5b03b4a346aa8387de992e325fc31f2498d2d
Nassim Jahnke <jahnke.nassim@gmail.com>
2022-04-03 10:18:06 +0200

    Fix user header tag edit button and text wrapping

--- frontend-new/src/components/UserHeader.vue
+++ frontend-new/src/components/UserHeader.vue
@@ -26,12 +26,12 @@ const props = defineProps<{
 const i18n = useI18n();
 const authStore = useAuthStore();
 
-const isCurrentUser = computed(() => {
-  return authStore.user && authStore.user.name === props.user.name;
+const isCurrentUser = computed<boolean>(() => {
+  return authStore.user !== null && authStore.user.name === props.user.name;
 });
 
-const canEditCurrentUser = computed(() => {
-  return hasPerms(NamedPermission.EDIT_ALL_USER_SETTINGS) || isCurrentUser || hasPerms(NamedPermission.EDIT_SUBJECT_SETTINGS);
+const canEditCurrentUser = computed<boolean>(() => {
+  return hasPerms(NamedPermission.EDIT_ALL_USER_SETTINGS) || isCurrentUser.value || hasPerms(NamedPermission.EDIT_SUBJECT_SETTINGS);
 });
 
 interface UserButton {
@@ -73,7 +73,7 @@ const buttons = computed<UserButton[]>(() => {
         <!-- todo org avatar changing -->
       </div>
 
-      <div class="ml-2">
+      <div class="ml-2 overflow-clip">
         <h1 class="text-2xl text-strong inline-flex items-center">
           {{ user.name }}
           <a v-if="!user.isOrganization" class="inline-flex mx-1" :href="forumUserUrl(user.name)" :title="i18n.t('author.viewOnForums')"
@@ -84,7 +84,11 @@ const buttons = computed<UserButton[]>(() => {
         <div>
           <span v-if="user.tagline">{{ user.tagline }}</span>
           <span v-else-if="canEditCurrentUser">{{ i18n.t("author.addTagline") }}</span>
-          <TaglineModal :tagline="user.tagline" :action="`${user.isOrganization ? 'organizations/org' : 'users'}/${user.name}/settings/tagline`" />
+          <TaglineModal
+            v-if="canEditCurrentUser"
+            :tagline="user.tagline"
+            :action="`${user.isOrganization ? 'organizations/org' : 'users'}/${user.name}/settings/tagline`"
+          />
         </div>
 
         <Tooltip v-for="btn in buttons" :key="btn.name" :content="i18n.t(`author.tooltips.${btn.name}`)">
@@ -97,7 +101,7 @@ const buttons = computed<UserButton[]>(() => {
       </div>
 
       <div class="flex-grow" />
-      <div class="flex flex-col space-y-1 items-end">
+      <div class="flex flex-col space-y-1 items-end flex-shrink-0">
         <span>{{ i18n.t("author.memberSince", [i18n.d(user.joinDate, "date")]) }}</span>
         <span>{{ i18n.t("author.numProjects", [user.projectCount], user.projectCount) }}</span>
         <span v-for="role in user.roles" :key="role.roleId" :style="{ backgroundColor: role.color }" class="rounded p-1 w-max items-end">{{ role.title }}</span>
