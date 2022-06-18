f566b22cef75cc3225aea8f73ba00cc913a82ff7
Alessio Gravili <alessio@gravili.de>
2022-06-19 01:04:08 +0200

    rename

--- frontend/components/logos/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as Paper } from './PaperLogo.vue';
-export { default as Velocity } from './VelocityLogo.vue';
-export { default as Waterfall } from './WaterfallLogo.vue';
--- frontend/components/users/index.ts
+++
@@ -1,1 +0,0 @@
-export { default as UserAvatar } from './UserAvatar.vue';
