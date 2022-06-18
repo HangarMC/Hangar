a9340e2004832620cd0bf1a4d1941eb85f47e3f1
MiniDigger | Martin <admin@benndorf.dev>
2022-06-18 11:46:36 +0200

    fix switching between org and users

--- frontend-new/src/layouts/default.vue
+++ frontend-new/src/layouts/default.vue
@@ -3,6 +3,11 @@ import Header from "~/components/layout/Header.vue";
 import Footer from "~/components/layout/Footer.vue";
 import Container from "~/components/design/Container.vue";
 import Notifications from "~/components/Notifications.vue";
+import { computed } from "vue";
+import { useRoute } from "vue-router";
+
+const route = useRoute();
+const key = computed<string>(() => route.params.user as string);
 </script>
 
 <template>
@@ -10,7 +15,7 @@ import Notifications from "~/components/Notifications.vue";
     <Header />
     <Container class="min-h-[80vh]">
       <Suspense>
-        <router-view v-slot="{ Component }" v-bind="$attrs">
+        <router-view v-slot="{ Component }" v-bind="$attrs" :key="key">
           <transition name="slide">
             <!-- dummy diff to make the transition work on pages where template root has multiple elements -->
             <div id="#page">
