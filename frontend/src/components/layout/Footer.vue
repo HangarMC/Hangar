0fa910b246f7992956ccb25cfbbd34ee0fe7e5e6
Alessio Gravili <alessio@gravili.de>
2022-06-19 15:19:02 +0200

    Improve footer style, mainly on mobile

--- frontend/src/components/layout/Footer.vue
+++ frontend/src/components/layout/Footer.vue
@@ -7,8 +7,7 @@ const i18n = useI18n();
 <template>
   <footer class="relative flex items-end mt-10 background-default px-8 pb-2 text-light-10 min-h-10">
     <div class="footerContent w-screen">
-      <div class="footerBar flex items-center justify-around mt-4 max-w-1200px m-auto text-sm">
-        <p>© 2022 <a href="https://papermc.io/">PaperMC</a></p>
+      <div class="footerBar flex flex-col gap-4 md:(flex-row-reverse gap-0) items-center justify-around mt-4 max-w-1200px m-auto text-sm">
         <div class="footerBarLinks flex flex-col md:flex-row">
           <router-link :to="{ name: 'guidelines' }" class="flex items-center rounded-md px-6 py-2" hover="text-primary-400 bg-primary-0"
             >{{ i18n.t("hangar.footer.resourceGuidelines") }}
@@ -23,6 +22,7 @@ const i18n = useI18n();
             >{{ i18n.t("hangar.footer.tos") }}
           </router-link>
         </div>
+        <p class="font-bold">© 2022 <a href="https://papermc.io/">PaperMC</a></p>
       </div>
     </div>
   </footer>
