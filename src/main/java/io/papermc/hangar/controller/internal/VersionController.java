78393e32c7860dece9e3d575a8376dbada6496e8
Nassim Jahnke <jahnke.nassim@gmail.com>
2022-06-20 10:03:21 +0200

    Make lastdependencies endpoint less strict

--- src/main/java/io/papermc/hangar/controller/internal/VersionController.java
+++ src/main/java/io/papermc/hangar/controller/internal/VersionController.java
@@ -199,7 +199,7 @@ public class VersionController extends HangarComponent {
     }
 
     @VisibilityRequired(type = Type.PROJECT, args = "{#author, #slug}")
-    @RateLimit(overdraft = 5, refillTokens = 2, refillSeconds = 10)
+    @RateLimit(overdraft = 5, refillTokens = 3, greedy = true)
     @GetMapping(path = "/version/{author}/{slug}/lastdependencies")
     public ResponseEntity<LastDependencies> getLastVersionDependencies(@PathVariable String author, @PathVariable String slug, @RequestParam(required = false) String channel, @RequestParam String platform) {
         return ResponseEntity.ok(versionService.getLastVersionDependencies(author, slug, channel, platform));
