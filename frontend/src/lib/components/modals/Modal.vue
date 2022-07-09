fe660c473ac82109d7c5b84a2e41d344b88e5070
MiniDigger | Martin <admin@benndorf.dev>
2022-07-09 14:46:14 +0200

    move modals to lib

---
+++ frontend/src/lib/components/modals/Modal.vue
@@ -0,0 +1,51 @@
+<script lang="ts" setup>
+import { ref } from "vue";
+import { Dialog, DialogOverlay } from "@headlessui/vue";
+
+const props = withDefaults(
+  defineProps<{
+    title: string;
+    small?: boolean;
+    big?: boolean;
+  }>(),
+  {
+    small: true,
+    big: false,
+  }
+);
+
+const isOpen = ref<boolean>(false);
+
+function open() {
+  isOpen.value = true;
+}
+
+function close() {
+  isOpen.value = false;
+}
+
+defineExpose({
+  open: open,
+  close: close,
+  isOpen: isOpen,
+});
+</script>
+
+<template>
+  <client-only>
+    <Dialog :open="isOpen" class="fixed inset-0 z-10 overflow-y-auto" @close="close">
+      <div class="flex items-center justify-center min-h-screen">
+        <DialogOverlay class="fixed inset-0 bg-black opacity-60" />
+
+        <div class="relative mx-auto background-default rounded p-4" :class="big ? 'max-w-80vw' : small ? 'max-w-sm' : 'max-w-lg'">
+          <div class="inline-flex items-center w-full pb-4 pr-1 text-xl">
+            <IconMdiClose class="cursor-pointer mr-1" @click="close" />
+            <h2 class="font-bold">{{ props.title }}</h2>
+          </div>
+          <slot :on="{ click: close }"></slot>
+        </div>
+      </div>
+    </Dialog>
+  </client-only>
+  <slot name="activator" :on="{ click: open }"></slot>
+</template>
---
+++ frontend/src/lib/components/modals/TextAreaModal.vue
@@ -0,0 +1,38 @@
+<script lang="ts" setup>
+import { useI18n } from "vue-i18n";
+import Button from "~/lib/components/design/Button.vue";
+import Modal from "~/lib/components/modals/Modal.vue";
+import { ref } from "vue";
+import InputTextarea from "~/lib/components/ui/InputTextarea.vue";
+
+const props = defineProps<{
+  title: string;
+  label: string;
+  submit: (msg: string) => Promise<void>;
+}>();
+
+const message = ref("");
+const loading = ref(false);
+
+const i18n = useI18n();
+
+async function _submit(close: () => void) {
+  loading.value = true;
+  await props.submit(message.value);
+  loading.value = false;
+  close();
+}
+</script>
+
+<template>
+  <Modal :title="props.title">
+    <template #default="{ on }">
+      <InputTextarea v-model.trim="message" :label="label" :rows="2" @keydown.enter.prevent="" />
+
+      <Button class="mt-3" :disabled="loading" @click="_submit(on.click)">{{ i18n.t("general.submit") }}</Button>
+    </template>
+    <template #activator="{ on }">
+      <slot name="activator" :on="on"></slot>
+    </template>
+  </Modal>
+</template>
