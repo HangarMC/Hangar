50ecd9dedafce1a8e70740a3741a8e49eee307c9
Nassim Jahnke <jahnke.nassim@gmail.com>
2022-07-24 13:25:37 +0200

    Fix pinned version download button

--- frontend/src/components/projects/DownloadButton.vue
+++ frontend/src/components/projects/DownloadButton.vue
@@ -68,7 +68,7 @@ const external = computed(() => false);
         rel="noopener noreferrer"
       >
         <PlatformLogo :platform="p" :size="24" class="mr-1" />
-        {{ backendData.platforms?.get(platform).name }}
+        {{ backendData.platforms?.get(p).name }}
         <span v-if="showVersions" class="ml-1">({{ formatVersionNumbers(v) }})</span>
       </DropdownItem>
     </DropdownButton>
