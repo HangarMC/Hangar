9773e826a2cca14c496c65d50a51dc2c16edc90e
MiniDigger | Martin <admin@benndorf.dev>
2022-07-26 11:09:02 +0200

    load swagger from cdn (#749)

--- frontend/src/pages/api.vue
+++ frontend/src/pages/api.vue
@@ -1,23 +1,31 @@
 <script lang="ts" setup>
 import { onMounted } from "vue";
-import { SwaggerConfigs, SwaggerUIBundle } from "swagger-ui-dist";
 import { useHead } from "@vueuse/head";
 import { useSeo } from "~/composables/useSeo";
 import { useI18n } from "vue-i18n";
 import { useRoute } from "vue-router";
 import { useAuthStore } from "~/store/auth";
 
-declare global {
-  interface Window {
-    ui: SwaggerUIBundle;
-  }
-}
-
 const i18n = useI18n();
 const route = useRoute();
 const authStore = useAuthStore();
 
 onMounted(() => {
+  const swaggerUiVersion = "4.13.0";
+
+  const bundle = document.createElement("script");
+  bundle.src = `https://unpkg.com/swagger-ui-dist@${swaggerUiVersion}/swagger-ui-bundle.js`;
+  bundle.crossOrigin = "";
+  document.body.append(bundle);
+
+  const preset = document.createElement("script");
+  preset.src = `https://unpkg.com/swagger-ui-dist@${swaggerUiVersion}/swagger-ui-standalone-preset.js`;
+  preset.crossOrigin = "";
+  document.body.append(preset);
+
+  const script = document.createElement("script");
+  // language=JavaScript
+  script.innerHTML = `
   window.ui = SwaggerUIBundle({
     url: "/v2/api-docs/",
     dom_id: "#swagger-ui",
@@ -32,8 +40,11 @@ onMounted(() => {
         }
       }
       return req;
-    },
-  } as SwaggerConfigs);
+    }
+  });
+  `;
+
+  bundle.onload = () => document.body.append(script);
 });
 
 useHead(useSeo(i18n.t("apiDocs.title"), null, route, null));
