18684a2697e8eea8ff7e683bc9093fd4200a42ac
MiniDigger | Martin <admin@benndorf.dev>
2022-07-30 14:08:23 +0200

    try to find out why shit is bork

--- src/main/java/io/papermc/hangar/util/RequestUtil.java
+++ src/main/java/io/papermc/hangar/util/RequestUtil.java
@@ -6,16 +6,29 @@ import org.springframework.web.server.ResponseStatusException;
 import javax.servlet.http.HttpServletRequest;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
+import java.util.Collections;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class RequestUtil {
 
     private RequestUtil() { }
 
     public static String getRemoteAddress(HttpServletRequest request) {
+        String headers = Collections.list(request.getHeaderNames())
+            .stream()
+            .collect(Collectors.toMap(
+                Function.identity(),
+                h -> Collections.list(request.getHeaders(h))
+            )).toString();
+        System.out.println("getRemoteAddress from " + headers);
+
         String header = request.getHeader("X-Forwarded-For");
         if (header == null) {
+            System.out.println("fall back to " + request.getRemoteAddr());
             return request.getRemoteAddr();
         } else {
+            System.out.println("found " + header);
             return header;
         }
     }
