5e57fa0a0eb9aa3447a8c2a2327d366073e0b9bd
MiniDigger | Martin <admin@benndorf.dev>
2022-07-31 11:53:07 +0200

    allow trusting domains in linkout, closes #810

--- frontend/src/pages/linkout.vue
+++ frontend/src/pages/linkout.vue
@@ -6,10 +6,30 @@ import Card from "~/lib/components/design/Card.vue";
 import { useI18n } from "vue-i18n";
 import Button from "~/lib/components/design/Button.vue";
 import Link from "~/lib/components/design/Link.vue";
+import { useLocalStorage } from "@vueuse/core";
+import { computed } from "vue";
 
 const route = useRoute();
 const remoteUrl = route.query.remoteUrl;
 const i18n = useI18n();
+
+const trustedHosts = useLocalStorage("trustedHosts", [] as string[]);
+const host = computed<string | null>(() => (remoteUrl ? new URL(remoteUrl as string).host : null));
+
+if (host.value && trustedHosts.value.includes(host.value)) {
+  go();
+}
+
+function trust() {
+  if (host.value) {
+    trustedHosts.value.push(host.value);
+  }
+  go();
+}
+
+function go() {
+  location.href = remoteUrl as string;
+}
 
 useHead(useSeo(i18n.t("linkout.title"), null, route, null));
 </script>
@@ -20,7 +40,10 @@ useHead(useSeo(i18n.t("linkout.title"), null, route, null));
     </template>
     {{ i18n.t("linkout.text", [remoteUrl]) }}
     <template #footer>
-      <Link :href="remoteUrl" target="_self" rel="noopener noreferrer">
+      <Link @click="trust">
+        <Button size="medium">{{ i18n.t("linkout.trust") }}</Button>
+      </Link>
+      <Link class="ml-2" :href="remoteUrl" target="_self" rel="noopener noreferrer">
         <Button size="medium">{{ i18n.t("linkout.continue") }}</Button>
       </Link>
       <Button button-type="secondary" size="medium" class="ml-2" @click="$router.back()">{{ i18n.t("linkout.abort") }}</Button>
