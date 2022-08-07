3ae41aa46c308cc2ab5290f05c6743c1c6c38e14
MiniDigger | Martin <admin@benndorf.dev>
2022-08-07 11:45:24 +0200

    start to toy around with s3 sdk

--- src/main/java/io/papermc/hangar/service/internal/file/S3FileService.java
+++ src/main/java/io/papermc/hangar/service/internal/file/S3FileService.java
@@ -5,52 +5,85 @@ import org.springframework.core.io.FileSystemResource;
 import org.springframework.stereotype.Service;
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.URI;
+import java.net.URISyntaxException;
+import io.papermc.hangar.config.hangar.StorageConfig;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.DeleteObjectRequest;
+import software.amazon.awssdk.services.s3.model.HeadObjectRequest;
 
 @Service
 @ConditionalOnProperty(value = "hangar.storage.type", havingValue = "object")
 public class S3FileService implements FileService {
+
+    private final StorageConfig config;
+    private final S3Client client;
+
+    public S3FileService(StorageConfig config) throws URISyntaxException {
+        this.config = config;
+        this.client = S3Client.builder()
+            .endpointOverride(new URI(config.getObjectStorageEndpoint()))
+            .credentialsProvider(StaticCredentialsProvider.create(AwsBasicCredentials.create(config.getAccessKey(), config.getSecretKey())))
+            .region(Region.of("local"))
+            .build();
+    }
+
     @Override
     public FileSystemResource getResource(String path) {
-        throw new UnsupportedOperationException();
+        throw new UnsupportedOperationException();// TODO
     }
 
     @Override
     public boolean exists(String path) {
-        throw new UnsupportedOperationException();
+        client.headObject(HeadObjectRequest.builder().build());
+        throw new UnsupportedOperationException();// TODO
     }
 
     @Override
     public void deleteDirectory(String dir) {
-        throw new UnsupportedOperationException();
+        throw new UnsupportedOperationException();// TODO
     }
 
     @Override
     public boolean delete(String path) {
-        throw new UnsupportedOperationException();
+        try {
+            client.deleteObject(DeleteObjectRequest.builder().bucket(config.getBucket()).key(path).build());
+            return true;
+        } catch (Exception ex) {
+            // TODO does this make remotely sense?
+            return false;
+        }
     }
 
     @Override
     public byte[] bytes(String path) throws IOException {
-        throw new UnsupportedOperationException();
+        throw new UnsupportedOperationException(); // TODO
     }
 
     @Override
     public void write(InputStream inputStream, String path) throws IOException {
-        throw new UnsupportedOperationException();
+        throw new UnsupportedOperationException();// TODO
     }
 
     @Override
     public void move(String oldPath, String newPath) throws IOException {
-        throw new UnsupportedOperationException();
+        throw new UnsupportedOperationException();// TODO
     }
 
     @Override
     public void link(String existingPath, String newPath) throws IOException {
-        throw new UnsupportedOperationException();
+        throw new UnsupportedOperationException();// TODO
     }
 
     @Override
     public String resolve(String path, String fileName) {
-        throw new UnsupportedOperationException();
+        if (path.endsWith("/")) {
+            return path + fileName;
+        } else {
+            return path + "/" + fileName;
+        }
     }
 }
