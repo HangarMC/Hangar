d83b0ec813d0251dbc62c566e2c8ed6a8ab45d80
MiniDigger | Martin <admin@benndorf.dev>
2022-09-11 21:25:40 +0200

    configure new staging and prod

--- frontend/src/lib/composables/useConfig.ts
+++ frontend/src/lib/composables/useConfig.ts
@@ -23,10 +23,36 @@ const configs: Record<string, Config> = {
     paypalEnv: "production",
     paypalIpn: "https://hangar.benndorf.dev/api/internal/paypal/ipn",
   },
+  "papermc.dev": {
+    publicHost: "https://hangar.papermc.dev",
+    proxyHost: "http://hangar-backend:8080",
+    authHost: "https://hangarauth.papermc.dev",
+    paypalEnv: "production",
+    paypalIpn: "https://hangar.papermc.dev/api/internal/paypal/ipn",
+  },
+  "papermc.io": {
+    publicHost: "https://hangar.papermc.io",
+    proxyHost: "http://hangar-backend:8080",
+    authHost: "https://hangarauth.papermc.io",
+    paypalEnv: "production",
+    paypalIpn: "https://hangar.papermc.io/api/internal/paypal/ipn",
+  },
 };
 
 export function useConfig(): Config {
-  const key = import.meta.env.SSR ? process.env.HANGAR_CONFIG_ENV || "localhost" : location.hostname.replace("auth.", "");
+  let key: string;
+  if (import.meta.env.SSR) {
+    key = process.env.HANGAR_CONFIG_ENV || "localhost";
+  } else {
+    const split = location.hostname.split(".");
+    if (split.length === 1) {
+      key = split[0];
+    } else if (split.length === 3) {
+      key = split[1] + "." + split[2];
+    } else {
+      key = location.hostname;
+    }
+  }
   configLog("useConfig", key);
   return configs[key];
 }
