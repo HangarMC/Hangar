58674c57d326b3206e72258a7d154919cd3aeacd
MiniDigger | Martin <admin@benndorf.dev>
2022-12-20 11:31:22 +0100

    fix: attribute styles not working

--- frontend/windi.config.ts
+++ frontend/windi.config.ts
@@ -2,5 +2,6 @@ import { defineConfig } from "vite-plugin-windicss";
 import WindiConfig from "./src/lib/config/windi.config.js";
 
 export default defineConfig({
+  attributify: true,
   presets: [WindiConfig],
 });
