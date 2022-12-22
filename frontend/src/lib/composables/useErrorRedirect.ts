f44e8449d706873053e81131753081ca612a5d59
MiniDigger | Martin <admin@benndorf.dev>
2022-12-22 18:48:42 +0100

    fix(frontend): fixup error handling

--- frontend/src/lib/composables/useErrorRedirect.ts
+++ frontend/src/lib/composables/useErrorRedirect.ts
@@ -1,17 +1,9 @@
 import { RouteLocationNamedRaw, RouteLocationNormalized } from "vue-router";
+import { createError } from "#imports";
 
 export function useErrorRedirect(currentRoute: RouteLocationNormalized, status: number, msg?: string): RouteLocationNamedRaw {
-  return {
-    name: "error",
-    params: {
-      // Match the path of your current page and keep the same url...
-      pathMatch: currentRoute.path.split("/").slice(1),
-      // pass other params
-      status,
-      msg,
-    },
-    // ...and the same query and hash.
-    query: currentRoute.query,
-    hash: currentRoute.hash,
-  };
+  throw createError({
+    status,
+    message: msg,
+  });
 }
