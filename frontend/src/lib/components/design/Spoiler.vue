ed66705954c0cae0e9afeaf4f01f5d6ed80ce3f8
Nassim Jahnke <nassim@njahnke.dev>
2022-12-31 17:43:24 +0100

    Update version page to show all platforms at once

---
+++ frontend/src/lib/components/design/Spoiler.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+const props = withDefaults(
+  defineProps<{
+    withLine?: boolean;
+  }>(),
+  {
+    withLine: true,
+  }
+);
+</script>
+
+<template>
+  <details open class="spoiler-details">
+    <summary>
+      <slot name="title" />
+    </summary>
+    <hr v-if="withLine" class="py-1" />
+    <slot name="content" />
+  </details>
+</template>
+
+<style>
+.spoiler-details {
+  border: 1px solid #bbb;
+  border-radius: 5px;
+  padding: 0.6rem 0.6rem 0.4rem;
+  max-width: 50%;
+  min-width: 300px;
+}
+
+.spoiler-details summary {
+  cursor: pointer;
+  list-style: none;
+  padding: 0.25rem;
+}
+</style>
