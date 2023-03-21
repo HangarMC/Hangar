01b53d690523c50bed714e8f6fdd57526ab3e053
Nassim Jahnke <nassim@njahnke.dev>
2023-03-21 12:04:18 +0100

    Hide version warning on partial approvals as well

--- frontend/src/components/projects/ProjectHeader.vue
+++ frontend/src/components/projects/ProjectHeader.vue
@@ -79,7 +79,7 @@ function requiresConfirmation(): ConfirmationType {
   for (const platform in props.project.mainChannelVersions) {
     const version = props.project.mainChannelVersions[platform as Platform];
     const download = version.downloads[platform as Platform];
-    if (version.reviewState !== ReviewState.REVIEWED) {
+    if (version.reviewState !== ReviewState.REVIEWED && version.reviewState !== ReviewState.PARTIALLY_REVIEWED) {
       return ConfirmationType.REQUIRED;
     }
     if (download && download.externalUrl !== null) {
