4b5f2fe96445cb7549fa354514cb680fa74cb237
Nassim Jahnke <nassim@njahnke.dev>
2023-03-30 09:26:58 +0200

    Fix relative anchors when opening in new page Doesn't work on page reload still, but good enough Closes #1131

--- frontend/src/app/router.options.ts
+++ frontend/src/app/router.options.ts
@@ -2,7 +2,15 @@ import { RouterConfig } from "@nuxt/schema";
 
 export default {
   scrollBehavior(to, from, savedPosition) {
-    if (to.path === from.path) return {};
+    if (to.path === from.path) {
+      return {};
+    }
+
+    const anchorIndex = to.fullPath.indexOf("#");
+    if (anchorIndex !== -1) {
+      return {};
+    }
+
     return savedPosition
       ? new Promise((resolve) => {
           setTimeout(() => {
