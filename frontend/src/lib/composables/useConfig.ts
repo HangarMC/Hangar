95be2babd72f1de3a2fa6178f7ffe157a0a633b6
MiniDigger | Martin <admin@benndorf.dev>
2023-04-02 02:33:56 +0200

    cleanup

--- frontend/src/lib/composables/useConfig.ts
+++ frontend/src/lib/composables/useConfig.ts
@@ -3,7 +3,6 @@ import { configLog } from "~/lib/composables/useLog";
 interface Config {
   publicHost: string;
   proxyHost: string;
-  authHost: string;
   paypalEnv: string;
   paypalIpn: string;
 }
@@ -12,28 +11,24 @@ const configs: Record<string, Config> = {
   localhost: {
     publicHost: "http://localhost:3333",
     proxyHost: "http://localhost:8080",
-    authHost: "http://localhost:3001",
     paypalEnv: "sandbox",
     paypalIpn: "https://hangar.benndorf.dev/api/internal/paypal/ipn",
   },
   "hangar.test": {
     publicHost: "https://hangar.test",
     proxyHost: "http://hangar-backend:8080",
-    authHost: "https://auth.hangar.test",
     paypalEnv: "production",
     paypalIpn: "https://hangar.benndorf.dev/api/internal/paypal/ipn",
   },
   "papermc.dev": {
     publicHost: "https://hangar.papermc.dev",
     proxyHost: "http://hangar-backend:8080",
-    authHost: "https://hangarauth.papermc.dev",
     paypalEnv: "production",
     paypalIpn: "https://hangar.papermc.dev/api/internal/paypal/ipn",
   },
   "papermc.io": {
     publicHost: "https://hangar.papermc.io",
     proxyHost: "http://hangar-backend:8080",
-    authHost: "https://hangarauth.papermc.io",
     paypalEnv: "production",
     paypalIpn: "https://hangar.papermc.io/api/internal/paypal/ipn",
   },
@@ -51,6 +46,20 @@ export function useConfig(): Config {
       key = split[1] + "." + split[2];
     } else {
       key = location.hostname;
+    }
+
+    if (key.startsWith("192.168")) {
+      const local = configs.localhost;
+      local.publicHost = local.publicHost.replace("http://localhost", "https://" + key);
+      local.proxyHost = local.proxyHost.replace("http://localhost", "https://" + key);
+      configLog("useConfig", key);
+      return local;
+    } else if (key.endsWith(".ngrok.io")) {
+      const local = configs.localhost;
+      local.publicHost = local.publicHost.replace("http://localhost", "https://" + key);
+      local.proxyHost = local.proxyHost.replace("http://localhost", "https://" + key);
+      configLog("useConfig", key);
+      return local;
     }
   }
   configLog("useConfig", key);
