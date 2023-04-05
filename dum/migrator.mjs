da564df82d749d7fff76786307ac0fc990300154
MiniDigger | Martin <admin@benndorf.dev>
2023-04-05 20:27:15 +0200

    fix: let migrator create hangar users for auth users who never logged in

--- dum/migrator.mjs
+++ dum/migrator.mjs
@@ -3,13 +3,15 @@ import pg from "pg";
 const {Pool} = pg;
 
 const kratosPool = new Pool({
-    connectionString: "postgresql://hangarauth:<pw>@localhost:1234/hangarauth",
+    connectionString: "postgresql://hangarauth:@localhost:1234/hangarauth",
     ssl: {rejectUnauthorized: false}
     //connectionString: "postgresql://hangar:hangar@localhost:5432/kratos",
 });
 
 const hangarPool = new Pool({
-    connectionString: "postgresql://hangar:hangar@localhost:5432/hangar",
+    connectionString: "postgresql://hangar:@localhost:1234/hangar",
+    ssl: {rejectUnauthorized: false}
+    //connectionString: "postgresql://hangar:hangar@localhost:5432/hangar",
 });
 
 const res = await kratosPool.query(`
@@ -26,7 +28,6 @@ const res = await kratosPool.query(`
         JOIN identity_verifiable_addresses iva ON i.id = iva.identity_id
         JOIN identity_credentials ic ON i.id = ic.identity_id
         JOIN identity_credential_types ict ON ict.id = ic.identity_credential_type_id
-    WHERE i.traits ->> 'username' = 'MiniDigger'
     ORDER BY username
 `);
 console.log("queried " + res.rows.length + " rows!");
@@ -41,42 +42,52 @@ for (let row of res.rows) {
         WHERE uuid = $1
     `, [row.uuid]);
 
-    const userId = user?.rows?.[0]?.id || 2;
+    let userId = user?.rows?.[0]?.id;
     if (!userId) {
-        console.error("Unknown user " + row.username + "?!", user.rows);
-        console.error(row);
-    } else {
-        try {
-            await hangarPool.query("BEGIN");
-
-            let type;
-            if (row.cred_type === "password") {
-                type = 0;
-            } else if (row.cred_type === "lookup_secret") {
-                type = 1;
-            } else if (row.cred_type === "totp") {
-                type = 2;
-            } else if (row.cred_type === "webauthn") {
-                type = 3;
-            }
-
-            await hangarPool.query(`
-                INSERT INTO user_credentials(created_at, user_id, credential, type, updated_at)
-                VALUES ($1, $2, $3, $4, $5)
-                ON CONFLICT (user_id, type) DO UPDATE SET created_at = $1, credential = $3, updated_at = $5
-            `, [row.created_at, userId, row.creds, type, row.updated_at]);
-
-            const verified = row.email_verified === "completed";
-            await hangarPool.query(`UPDATE users
-                                    SET email_verified = $1, socials = $2
-                                    WHERE id = $3`, [verified, row.socials, userId])
-
-            await hangarPool.query("COMMIT");
-        } catch (e) {
-            await hangarPool.query("ROLLBACK");
-            console.error("Failed to migrate " + row.username + " " + row.cred_type);
-            console.error(e);
+        console.warn("Unknown user " + row.username + ", creating", user.rows);
+        const verified = row.email_verified === "completed";
+        const insert = await hangarPool.query(`
+            INSERT INTO users(uuid, created_at, name, email, join_date, email_verified, socials)
+            VALUES ($1, $2, $3, $4, $5, $6, $7)
+            RETURNING id
+        `, [row.uuid, row.created_at, row.username, row.email, row.created_at, verified, row.socials]);
+        userId = insert?.rows?.[0]?.id;
+        if (!userId) {
+            console.error("insert fucked?", row, insert);
+            continue;
         }
+    }
+
+    try {
+        await hangarPool.query("BEGIN");
+
+        let type;
+        if (row.cred_type === "password") {
+            type = 0;
+        } else if (row.cred_type === "lookup_secret") {
+            type = 1;
+        } else if (row.cred_type === "totp") {
+            type = 2;
+        } else if (row.cred_type === "webauthn") {
+            type = 3;
+        }
+
+        await hangarPool.query(`
+            INSERT INTO user_credentials(created_at, user_id, credential, type, updated_at)
+            VALUES ($1, $2, $3, $4, $5)
+            ON CONFLICT (user_id, type) DO UPDATE SET created_at = $1, credential = $3, updated_at = $5
+        `, [row.created_at, userId, row.creds, type, row.updated_at]);
+
+        const verified = row.email_verified === "completed";
+        await hangarPool.query(`UPDATE users
+                                SET email_verified = $1, socials = $2
+                                WHERE id = $3`, [verified, row.socials, userId])
+
+        await hangarPool.query("COMMIT");
+    } catch (e) {
+        await hangarPool.query("ROLLBACK");
+        console.error("Failed to migrate " + row.username + " " + row.cred_type);
+        console.error(e);
     }
 }
 
