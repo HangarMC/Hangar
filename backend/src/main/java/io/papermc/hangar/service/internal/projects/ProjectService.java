29f4cda47b5f44cddb1163536ca09a723ce91057
Nassim Jahnke <nassim@njahnke.dev>
2023-04-16 10:49:27 +0200

    Do not add refreshHomeProjects to transactions
    Fixes HANGAR-B

--- backend/src/main/java/io/papermc/hangar/service/internal/projects/ProjectService.java
+++ backend/src/main/java/io/papermc/hangar/service/internal/projects/ProjectService.java
@@ -56,6 +56,7 @@ import org.springframework.context.annotation.Lazy;
 import org.springframework.http.HttpStatus;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.server.ResponseStatusException;
 
@@ -268,6 +269,7 @@ public class ProjectService extends HangarComponent {
     }
 
     @Async
+    @Transactional(propagation = Propagation.NOT_SUPPORTED)
     public void refreshHomeProjects() {
         this.hangarProjectsDAO.refreshHomeProjects();
     }
