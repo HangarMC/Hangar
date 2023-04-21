9782e7ba0acf435edc7a96d1b6ec0bfe28896830
MiniDigger | Martin <admin@benndorf.dev>
2023-04-21 11:45:24 +0200

    fix: refresh project and version on route change (closes #1128)

--- frontend/src/pages/[user]/[project].vue
+++ frontend/src/pages/[user]/[project].vue
@@ -1,14 +1,14 @@
 <script lang="ts" setup>
-import { type PropType, provide } from "vue";
+import { type PropType, provide, watch } from "vue";
 import { User } from "hangar-api";
 import { useI18n } from "vue-i18n";
 import { useRoute } from "vue-router";
-import { HangarProjectPage } from "hangar-internal";
+import { HangarProjectPage, HangarProject } from "hangar-internal";
 import { useProject } from "~/composables/useApiHelper";
 import { useErrorRedirect } from "~/composables/useErrorRedirect";
 import ProjectHeader from "~/components/projects/ProjectHeader.vue";
 import ProjectNav from "~/components/projects/ProjectNav.vue";
-import { createError, navigateTo } from "#imports";
+import { createError, navigateTo, useInternalApi } from "#imports";
 import Delayed from "~/components/design/Delayed.vue";
 
 defineProps({
@@ -21,17 +21,29 @@ defineProps({
 const i18n = useI18n();
 const route = useRoute();
 const project = await useProject(route.params.user as string, route.params.project as string);
-if (!project || !project.value) {
-  throw useErrorRedirect(route, 404, "Not found");
-} else if (route.params.project !== project.value?.namespace.slug) {
-  const newPath = route.fullPath.replace(route.params.project as string, project.value?.namespace.slug);
-  console.debug("Redirect to " + newPath + " from (" + route.fullPath + ")");
-  await navigateTo(newPath);
-  throw createError("dummy");
+await verify();
+
+async function verify() {
+  if (!project?.value) {
+    throw useErrorRedirect(route, 404, "Not found");
+  } else if (route.params.project !== project.value?.namespace.slug) {
+    const newPath = route.fullPath.replace(route.params.project as string, project.value!.namespace.slug);
+    console.debug("Redirect to " + newPath + " from (" + route.fullPath + ")");
+    await navigateTo(newPath);
+    throw createError("dummy");
+  }
 }
 
+watch(
+  () => route.params.project,
+  async () => {
+    project.value = await useInternalApi<HangarProject>("projects/project/" + route.params.user + "/" + route.params.project);
+    await verify();
+  }
+);
+
 provide("updateProjectPages", function (pages: HangarProjectPage[]) {
-  if (project && project.value) project.value.pages = pages;
+  if (project && project.value) project.value!.pages = pages;
 });
 </script>
 
--- frontend/src/pages/[user]/[project]/versions/[version].vue
+++ frontend/src/pages/[user]/[project]/versions/[version].vue
@@ -1,11 +1,13 @@
 <script lang="ts" setup>
 import { useI18n } from "vue-i18n";
 import { useRoute } from "vue-router";
-import { HangarProject } from "hangar-internal";
+import { computed, watch } from "vue";
+import { HangarProject, HangarVersion } from "hangar-internal";
 import { useProjectVersionsInternal } from "~/composables/useApiHelper";
 import { useErrorRedirect } from "~/composables/useErrorRedirect";
 import { Platform } from "~/types/enums";
 import Delayed from "~/components/design/Delayed.vue";
+import { useInternalApi } from "~/composables/useApi";
 
 const i18n = useI18n();
 const route = useRoute();
@@ -15,16 +17,29 @@ const props = defineProps<{
 }>();
 
 const version = await useProjectVersionsInternal(route.params.user as string, route.params.project as string, route.params.version as string);
-if (!version?.value) {
-  throw useErrorRedirect(route, 404, "Not found");
-}
+verify();
 
-const versionPlatforms: Set<Platform> = new Set<Platform>();
-if (version && version.value) {
-  for (const platformKey in version.value.platformDependencies) {
-    versionPlatforms.add(platformKey as Platform);
+function verify() {
+  if (!version?.value) {
+    throw useErrorRedirect(route, 404, "Not found");
   }
 }
+
+watch(
+  () => route.params.version,
+  async () =>
+    (version.value = await useInternalApi<HangarVersion>(`versions/version/${route.params.user}/${route.params.project}/versions/${route.params.version}`))
+);
+
+const versionPlatforms = computed<Set<Platform>>(() => {
+  const result = new Set<Platform>();
+  if (version?.value) {
+    for (const platformKey in version.value!.platformDependencies) {
+      result.add(platformKey as Platform);
+    }
+  }
+  return result;
+});
 </script>
 
 <template>
