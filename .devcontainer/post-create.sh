ea673cb8a87b3ad4e702ae52ffe91520bed9de56
Gabriel Arriagada <65878291+GNosii@users.noreply.github.com>
2023-04-30 20:31:38 +0000

    prevent spam in console

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,10 +1,10 @@
 ## Sets up an bare-bones Hangar environment to make developing in Codespaces quicker and easier.
 
 ## Install Maven dependencies (backend)
-cd backend && mvn dependency:go-offline
+cd backend && mvn dependency:go-offline -q
 
 ## Install PNPM dependencies (frontend)
-cd ../frontend && pnpm i
+cd ../frontend && pnpm i --silent
 
 ## Create Docker containers (db & email)
-cd ../docker && docker-compose -f dev.yml up --no-start
+cd ../docker && docker-compose -f dev.yml up --no-start --quiet-pull
