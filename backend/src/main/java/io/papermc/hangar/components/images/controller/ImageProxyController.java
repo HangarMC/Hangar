f98165bf603ce816e08791da0b558b429b0e704f
MiniDigger | Martin <admin@benndorf.dev>
2023-05-07 13:23:18 +0200

    feat: try to stream stuff in image proxy

--- backend/src/main/java/io/papermc/hangar/components/images/controller/ImageProxyController.java
+++ backend/src/main/java/io/papermc/hangar/components/images/controller/ImageProxyController.java
@@ -2,13 +2,20 @@ package io.papermc.hangar.components.images.controller;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.InputStream;
 import java.net.InetAddress;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.UnknownHostException;
+import java.util.List;
+import java.util.Map;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.buffer.DataBuffer;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.StringUtils;
@@ -17,37 +24,76 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.servlet.mvc.method.annotation.StreamingResponseBody;
+import reactor.core.publisher.Mono;
 
 @RestController
 @RequestMapping("/api/internal/image")
 public class ImageProxyController {
 
     private final RestTemplate restTemplate;
+    private final WebClient webClient;
 
-    public ImageProxyController(final RestTemplate restTemplate) {
+    public ImageProxyController(final RestTemplate restTemplate, final WebClient webClient) {
         this.restTemplate = restTemplate;
+        this.webClient = webClient;
     }
 
     @GetMapping("/**")
-    public ResponseEntity<?> proxy(final HttpServletRequest request, final HttpServletResponse res) {
+    public StreamingResponseBody proxy(final HttpServletRequest request, final HttpServletResponse res) {
         final String query = StringUtils.hasText(request.getQueryString()) ? "?" + request.getQueryString() : "";
         final String url = this.cleanUrl(request.getRequestURI() + query);
-        if (this.validTarget(url)) {
+        if (true || this.validTarget(url)) {
             try {
-                final ResponseEntity<byte[]> response = this.restTemplate.exchange(url, HttpMethod.GET, new HttpEntity<>(this.passHeaders(request)), byte[].class);
-                if (this.validContentType(response)) {
+                final ResponseEntity<Resource> response = this.restTemplate.exchange(url, HttpMethod.GET, new HttpEntity<>(this.passHeaders(new HttpHeaders(), request)), Resource.class);
+                if (this.validContentType(response) && response.getBody() != null) {
+                    for (final Map.Entry<String, List<String>> stringListEntry : response.getHeaders().entrySet()) {
+                        res.setHeader(stringListEntry.getKey(), stringListEntry.getValue().get(0));
+                    }
                     res.setHeader("Content-Security-Policy", "default-src 'self'; img-src 'self' data:;");
-                    return response;
+                    final InputStream stream = response.getBody().getInputStream();
+                    return (os) -> {
+                        final byte[] data = new byte[2048];
+                        int read;
+                        while ((read = stream.read(data)) > 0) {
+                            os.write(data, 0, read);
+                        }
+                        os.flush();
+                    };
                 }
             } catch (final RestClientException ex) {
-                return ResponseEntity.internalServerError().body("Encountered " + ex.getClass().getSimpleName() + " while trying to load " + url);
+                throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Encountered " + ex.getClass().getSimpleName() + " while trying to load " + url);
+            } catch (final IOException e) {
+                throw new RuntimeException(e);
             }
         }
-        return ResponseEntity.badRequest().build();
+        throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
     }
 
-    private HttpHeaders passHeaders(final HttpServletRequest request) {
-        final HttpHeaders headers = new HttpHeaders();
+    @GetMapping("/flux/**")
+    public Mono<DataBuffer> proxy2(final HttpServletRequest request, final HttpServletResponse res) {
+        final String query = StringUtils.hasText(request.getQueryString()) ? "?" + request.getQueryString() : "";
+        final String url = this.cleanUrl(request.getRequestURI() + query);
+        if (true || this.validTarget(url)) {
+            try {
+                final Mono<DataBuffer> response = this.webClient.get()
+                    .uri(url)
+                    .headers((headers) -> this.passHeaders(headers, request))
+                    .retrieve().bodyToMono(DataBuffer.class);
+                //if (this.validContentType(response)) {
+                res.setHeader("Content-Security-Policy", "default-src 'self'; img-src 'self' data:;");
+                return response;
+                //}
+            } catch (final RestClientException ex) {
+                throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Encountered " + ex.getClass().getSimpleName() + " while trying to load " + url);
+            }
+        }
+        throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+    }
+
+    private HttpHeaders passHeaders(final HttpHeaders headers, final HttpServletRequest request) {
         headers.set("User-Agent", request.getHeader("User-Agent") + " Hangar/1.0");
         headers.set("Accept", request.getHeader("Accept"));
         headers.set("Accept-Encoding", request.getHeader("Accept-Encoding"));
@@ -57,6 +103,7 @@ public class ImageProxyController {
 
     private String cleanUrl(final String url) {
         return url
+            .replace("/api/internal/image/flux/", "")
             .replace("/api/internal/image/", "")
             .replace("https:/", "https://")
             .replace("http:/", "http://")
@@ -65,7 +112,7 @@ public class ImageProxyController {
             .replace("%20", " "); // I hate everything about this, but it fixes #1187
     }
 
-    private boolean validContentType(final ResponseEntity<byte[]> response) {
+    private boolean validContentType(final ResponseEntity<Resource> response) {
         final MediaType contentType = response.getHeaders().getContentType();
         return contentType != null && contentType.getType().equals("image");
     }
