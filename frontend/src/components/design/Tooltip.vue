e45e077533da782a396f6c1f8c52ce7abdd8d5ec
Nassim Jahnke <nassim@njahnke.dev>
2024-04-25 18:58:53 +0200

    Unfix console warning/hydration mismatch in Tooltip

--- frontend/src/components/design/Tooltip.vue
+++ frontend/src/components/design/Tooltip.vue
@@ -21,14 +21,12 @@ onErrorCaptured((err) => {
 </script>
 
 <template>
-  <ClientOnly>
-    <Popper :hover="hover" open-delay="200" close-delay="100" :show="show">
-      <slot />
-      <template #content>
-        <slot name="content" />
-      </template>
-    </Popper>
-  </ClientOnly>
+   <Popper :hover="hover" open-delay="200" close-delay="100" :show="show">
+     <slot />
+     <template #content>
+       <slot name="content" />
+     </template>
+   </Popper>
 </template>
 
 <style>
