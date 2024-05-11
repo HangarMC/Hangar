705d2724eba614fb67e758273a3086e1ccc3ac78
MiniDigger | Martin <admin@benndorf.dev>
2024-05-11 10:07:29 +0200

    fix: use css to hide headline in stepper

--- frontend/src/components/design/Steps.vue
+++ frontend/src/components/design/Steps.vue
@@ -101,7 +101,7 @@ async function goto(step: Step) {
     <div class="mt-4">
       <Card accent>
         <template #header>
-          <span v-show="settings.mobile">{{ activeStep?.header }}</span>
+          <span class="hidden md:block">{{ activeStep?.header }}</span>
         </template>
         <div v-for="step in steps" :key="step.value">
           <slot v-if="internalValue === step.value" :name="step.value" />
