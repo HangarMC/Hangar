89e331033e5bd3875ef752602a04e3ee5dd0457e
MiniDigger | Martin <admin@benndorf.dev>
2024-07-07 10:27:30 +0200

    fix: hack in a close button into the crowdin modal... (closes #1366)
    why this isn't a standard feature is beyond me

--- frontend/src/composables/useSeo.ts
+++ frontend/src/composables/useSeo.ts
@@ -92,6 +92,7 @@ export function useSeo(
       },
       {
         type: "text/javascript",
+        defer: true,
         src: "https://cdn.crowdin.com/jipt/jipt.js",
       }
     );
--- frontend/src/layouts/default.vue
+++ frontend/src/layouts/default.vue
@@ -1,5 +1,15 @@
 <script setup lang="ts">
 const route = useRoute();
+const i18n = useI18n();
+
+function fixCrowdin() {
+  document.documentElement.classList.remove("jipt-no-scroll");
+  document.body.classList.remove("jipt-highlight");
+  document.getElementById("crowdin-jipt-mask")?.remove();
+  document.getElementById("jipt-modal-mask")?.remove();
+  // eslint-disable-next-line unicorn/no-array-for-each
+  document.querySelectorAll(".crowdin-jipt")?.forEach((e) => e.remove());
+}
 </script>
 
 <template>
@@ -12,5 +22,21 @@ const route = useRoute();
     </Container>
     <Notifications />
     <Footer />
+    <button v-if="i18n.locale.value === 'dum'" class="crowdin-fix" aria-hidden="true" @click="fixCrowdin">✖</button>
   </main>
 </template>
+
+<style lang="scss" scoped>
+.crowdin-fix {
+  display: none;
+}
+.jipt-no-scroll .crowdin-fix {
+  position: absolute;
+  display: block;
+  top: 10px;
+  right: 10px;
+  z-index: 1000000000000000000;
+  color: black;
+  font-size: 26px;
+}
+</style>
