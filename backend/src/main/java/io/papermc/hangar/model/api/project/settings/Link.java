14d070f534878376f103d11ae5eb02b97fa93f25
MiniDigger | Martin <admin@benndorf.dev>
2024-07-20 09:10:27 +0200

    fix: prevent saving empty links

--- backend/src/main/java/io/papermc/hangar/model/api/project/settings/Link.java
+++ backend/src/main/java/io/papermc/hangar/model/api/project/settings/Link.java
@@ -1,7 +1,8 @@
 package io.papermc.hangar.model.api.project.settings;
 
 import io.papermc.hangar.controller.validations.Validate;
+import jakarta.validation.constraints.NotNull;
 
 // @el(root: String)
-public record Link(long id, @Validate(SpEL = "@validate.max(#root, @hangarConfig.pages.maxNameLen())") String name, @Validate(SpEL = "@validate.regex(#root, @hangarConfig.urlRegex)", message = "fieldError.url") String url) {
+public record Link(long id, @Validate(SpEL = "@validate.max(#root, @hangarConfig.pages.maxNameLen())") String name, @Validate(SpEL = "@validate.regex(#root, @hangarConfig.urlRegex)", message = "fieldError.url") @NotNull String url) {
 }
