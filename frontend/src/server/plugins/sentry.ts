94d4c9468ae81292766b2b1514e099c241c4a68f
MiniDigger | Martin <admin@benndorf.dev>
2024-07-27 16:45:54 +0200

    fix: disable nitro sentry integration for now

--- frontend/src/server/plugins/sentry.ts
+++ frontend/src/server/plugins/sentry.ts
@@ -1,5 +1,5 @@
-import * as Sentry from "@sentry/node";
-import { nodeProfilingIntegration } from "@sentry/profiling-node";
+// import * as Sentry from "@sentry/node";
+// import { nodeProfilingIntegration } from "@sentry/profiling-node";
 import { H3Error } from "h3";
 
 export default defineNitroPlugin((nitroApp) => {
@@ -12,35 +12,36 @@ export default defineNitroPlugin((nitroApp) => {
     return;
   }
 
+  // TODO make this work with bun
   let profiler = null;
   if (typeof Bun === "undefined") {
-    profiler = nodeProfilingIntegration();
+    // profiler = nodeProfilingIntegration();
   }
 
-  Sentry.init({
-    dsn: sentry.dsn,
-    environment: sentry.environment,
-    integrations: profiler ? [profiler] : [],
-    tracesSampleRate: 1,
-    profilesSampleRate: 0.2,
-  });
+  // Sentry.init({
+  //   dsn: sentry.dsn,
+  //   environment: sentry.environment,
+  //   integrations: profiler ? [profiler] : [],
+  //   tracesSampleRate: 1,
+  //   profilesSampleRate: 0.2,
+  // });
 
-  nitroApp.hooks.hook("error", (error) => {
-    // Do not handle 404s and 422s
-    if (error instanceof H3Error) {
-      if (error.statusCode === 404 || error.statusCode === 422) {
-        return;
-      }
-    }
-
-    Sentry.captureException(error);
-  });
-
-  nitroApp.hooks.hook("request", (event) => {
-    event.context.$sentry = Sentry;
-  });
-
-  nitroApp.hooks.hookOnce("close", async () => {
-    await Sentry.close(2000);
-  });
+  // nitroApp.hooks.hook("error", (error) => {
+  //   // Do not handle 404s and 422s
+  //   if (error instanceof H3Error) {
+  //     if (error.statusCode === 404 || error.statusCode === 422) {
+  //       return;
+  //     }
+  //   }
+  //
+  //   Sentry.captureException(error);
+  // });
+  //
+  // nitroApp.hooks.hook("request", (event) => {
+  //   event.context.$sentry = Sentry;
+  // });
+  //
+  // nitroApp.hooks.hookOnce("close", async () => {
+  //   await Sentry.close(2000);
+  // });
 });
