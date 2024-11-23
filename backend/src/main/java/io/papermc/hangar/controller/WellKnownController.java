149e4c158ae873f35c5aba6f0db7a2e507516327
MiniDigger | Martin <admin@minidigger.dev>
2024-11-23 11:51:15 +0100

    fix: /.well-known/change-password redirect works again

--- backend/src/main/java/io/papermc/hangar/controller/WellKnownController.java
+++ backend/src/main/java/io/papermc/hangar/controller/WellKnownController.java
@@ -3,19 +3,19 @@ package io.papermc.hangar.controller;
 import io.papermc.hangar.HangarComponent;
 import io.papermc.hangar.security.annotations.Anyone;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.ModelAndView;
 
 @Anyone
 @RestController
 @RequestMapping("/.well-known")
 public class WellKnownController extends HangarComponent {
 
-    @GetMapping(path = "/change-password", produces = MediaType.TEXT_PLAIN_VALUE)
-    public ModelAndView changePassword() {
-        return new ModelAndView("redirect:/auth/settings/account");
+    @GetMapping(path = "/change-password")
+    public ResponseEntity<Object> changePassword() {
+        return ResponseEntity.status(301).header("location", "/auth/settings/account").build();
     }
 
     @GetMapping(path = "/security.txt", produces = MediaType.TEXT_PLAIN_VALUE)
