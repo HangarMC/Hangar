e7c2ad9acdffb83a38a8bd7bcc3722880831582f
Julian Vennen <julian@vennen.me>
2025-01-08 15:32:48 +0100

    Accept project ids in permission endpoints

--- backend/src/main/java/io/papermc/hangar/controller/api/v1/PermissionsController.java
+++ backend/src/main/java/io/papermc/hangar/controller/api/v1/PermissionsController.java
@@ -8,9 +8,14 @@ import io.papermc.hangar.model.api.permissions.UserPermissions;
 import io.papermc.hangar.model.common.NamedPermission;
 import io.papermc.hangar.model.common.Permission;
 import io.papermc.hangar.model.common.PermissionType;
+import io.papermc.hangar.model.db.OrganizationTable;
+import io.papermc.hangar.model.db.projects.ProjectTable;
 import io.papermc.hangar.security.annotations.Anyone;
 import io.papermc.hangar.security.annotations.ratelimit.RateLimit;
 import io.papermc.hangar.service.PermissionService;
+import io.papermc.hangar.service.internal.organizations.OrganizationService;
+import io.papermc.hangar.service.internal.projects.ProjectService;
+import io.papermc.hangar.util.StringUtils;
 import java.util.Collection;
 import java.util.Set;
 import java.util.function.BiPredicate;
@@ -26,50 +31,81 @@ import org.springframework.stereotype.Controller;
 @RateLimit(path = "apipermissions", refillTokens = 100, greedy = true)
 public class PermissionsController extends HangarComponent implements IPermissionsController {
 
+    private final OrganizationService organizationService;
+    private final ProjectService projectService;
     private final PermissionService permissionService;
 
     @Autowired
-    public PermissionsController(final PermissionService permissionService) {
+    public PermissionsController(final OrganizationService organizationService, final ProjectService projectService, final PermissionService permissionService) {
+        this.organizationService = organizationService;
+        this.projectService = projectService;
         this.permissionService = permissionService;
     }
 
     @Override
-    public ResponseEntity<PermissionCheck> hasAllPermissions(final Set<NamedPermission> permissions, final String slug, final String organization) {
-        return this.has(permissions, slug, organization, (namedPerms, perm) -> namedPerms.stream().allMatch(p -> perm.has(p.getPermission())));
+    public ResponseEntity<PermissionCheck> hasAllPermissions(final Set<NamedPermission> permissions, final String project, final String organization) {
+        return this.has(permissions, project, organization, (namedPerms, perm) -> namedPerms.stream().allMatch(p -> perm.has(p.getPermission())));
     }
 
     @Override
-    public ResponseEntity<PermissionCheck> hasAny(final Set<NamedPermission> permissions, final String slug, final String organization) {
-        return this.has(permissions, slug, organization, (namedPerms, perm) -> namedPerms.stream().anyMatch(p -> perm.has(p.getPermission())));
+    public ResponseEntity<PermissionCheck> hasAny(final Set<NamedPermission> permissions, final String project, final String organization) {
+        return this.has(permissions, project, organization, (namedPerms, perm) -> namedPerms.stream().anyMatch(p -> perm.has(p.getPermission())));
     }
 
     @Override
-    public ResponseEntity<UserPermissions> showPermissions(final String slug, final String organization) {
-        final Pair<PermissionType, Permission> scopedPerms = this.getPermissionsInScope(slug, organization);
+    public ResponseEntity<UserPermissions> showPermissions(final String project, final String organization) {
+        final Pair<PermissionType, Permission> scopedPerms = this.getPermissionsInScope(project, organization);
         return ResponseEntity.ok(new UserPermissions(scopedPerms.getLeft(), scopedPerms.getRight().toBinString(), scopedPerms.getRight().toNamed()));
     }
 
-    private Pair<PermissionType, Permission> getPermissionsInScope(final String slug, final String organization) {
-        if (slug != null && organization == null) { // author & slug
-            Permission perms = this.permissionService.getProjectPermissions(this.getHangarUserId(), slug);
-            perms = this.getHangarPrincipal().getPossiblePermissions().intersect(perms);
-            return new ImmutablePair<>(PermissionType.PROJECT, perms);
-        } else if (slug == null && organization == null) { // current user (I don't think there's a need to see other user's global permissions)
-            Permission perms = this.permissionService.getGlobalPermissions(this.getHangarUserId());
-            perms = this.getHangarPrincipal().getPossiblePermissions().intersect(perms);
-            return new ImmutablePair<>(PermissionType.GLOBAL, perms);
-        } else if (slug == null) { // just org
-            Permission perms = this.permissionService.getOrganizationPermissions(this.getHangarUserId(), organization);
-            perms = this.getHangarPrincipal().getPossiblePermissions().intersect(perms);
-            return new ImmutablePair<>(PermissionType.ORGANIZATION, perms);
-        } else {
-            // unreachable
-            throw new HangarApiException(HttpStatus.BAD_REQUEST, "Incorrect request parameters");
+    private Pair<PermissionType, Permission> getPermissionsInScope(final String project, final String organization) {
+        if (project != null && organization != null) {
+            throw new HangarApiException(HttpStatus.BAD_REQUEST, "Specifying both project and organization is not allowed");
         }
+
+        if (project != null) {
+            ProjectTable projectTable = null;
+            if (StringUtils.isLong(project)) {
+                projectTable = this.projectService.getProjectTable(Long.parseLong(project));
+            }
+
+            if (projectTable == null) {
+                projectTable = this.projectService.getProjectTable(project);
+            }
+
+            if (projectTable == null) {
+                throw new HangarApiException(HttpStatus.NOT_FOUND, "Project not found");
+            }
+
+            final Permission perms = this.permissionService.getProjectPermissions(this.getHangarUserId(), projectTable.getId());
+            return new ImmutablePair<>(PermissionType.PROJECT, perms);
+        }
+
+        if (organization != null) {
+            OrganizationTable organizationTable = null;
+            if (StringUtils.isLong(organization)) {
+                organizationTable = this.organizationService.getOrganizationTable(Long.parseLong(organization));
+            }
+
+            if (organizationTable == null) {
+                organizationTable = this.organizationService.getOrganizationTable(organization);
+            }
+
+            if (organizationTable == null) {
+                throw new HangarApiException(HttpStatus.NOT_FOUND, "Organization not found");
+            }
+
+            final Permission perms = this.permissionService.getOrganizationPermissions(this.getHangarUserId(), organizationTable.getId());
+            return new ImmutablePair<>(PermissionType.ORGANIZATION, perms);
+        }
+
+        Permission perms = this.permissionService.getGlobalPermissions(this.getHangarUserId());
+        perms = this.getHangarPrincipal().getPossiblePermissions().intersect(perms);
+        return new ImmutablePair<>(PermissionType.GLOBAL, perms);
     }
 
-    private ResponseEntity<PermissionCheck> has(final Collection<NamedPermission> perms, final String slug, final String organization, final BiPredicate<Collection<NamedPermission>, Permission> check) {
-        final Pair<PermissionType, Permission> scopedPerms = this.getPermissionsInScope(slug, organization);
+    private ResponseEntity<PermissionCheck> has(final Collection<NamedPermission> perms, final String project, final String organization, final BiPredicate<Collection<NamedPermission>, Permission> check) {
+        final Pair<PermissionType, Permission> scopedPerms = this.getPermissionsInScope(project, organization);
         return ResponseEntity.ok(new PermissionCheck(scopedPerms.getLeft(), check.test(perms, scopedPerms.getRight())));
     }
 }
