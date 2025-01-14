9041064ad4d004b0db7748386ee1aeb849a667cb
Julian Vennen <julian@vennen.me>
2025-01-14 13:59:51 -0500

    Replace all Table parameters with strings in the OpenAPI schema (#1439)

--- backend/src/main/java/io/papermc/hangar/config/SwaggerConfig.java
+++ backend/src/main/java/io/papermc/hangar/config/SwaggerConfig.java
@@ -8,9 +8,7 @@ import io.papermc.hangar.controller.extras.pagination.annotations.ApplicableSort
 import io.papermc.hangar.exceptions.HangarApiException;
 import io.papermc.hangar.exceptions.MethodArgumentNotValidExceptionSerializer;
 import io.papermc.hangar.exceptions.MultiHangarApiException;
-import io.papermc.hangar.model.db.UserTable;
-import io.papermc.hangar.model.db.projects.ProjectTable;
-import io.papermc.hangar.model.db.versions.ProjectVersionTable;
+import io.papermc.hangar.model.db.Table;
 import io.swagger.v3.core.converter.ModelConverters;
 import io.swagger.v3.oas.models.Operation;
 import io.swagger.v3.oas.models.info.Info;
@@ -170,7 +168,7 @@ public class SwaggerConfig {
     public static class SlugOrIdCustomizer implements ParameterCustomizer {
         @Override
         public Parameter customize(final Parameter parameter, final MethodParameter methodParameter) {
-            if (Set.of(ProjectTable.class, ProjectVersionTable.class, UserTable.class).contains(methodParameter.getParameterType())) {
+            if (Table.class.isAssignableFrom(methodParameter.getParameterType())) {
                 return parameter.schema(new StringSchema());
             }
             return parameter;
