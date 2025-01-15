1c09118926c81325c562cf028a4cf0578be690f7
Julian Vennen <julian@vennen.me>
2025-01-15 12:46:10 +0100

    Add user id to Project Members

--- backend/src/main/java/io/papermc/hangar/model/api/project/ProjectMember.java
+++ backend/src/main/java/io/papermc/hangar/model/api/project/ProjectMember.java
@@ -3,5 +3,5 @@ package io.papermc.hangar.model.api.project;
 import io.papermc.hangar.model.common.roles.CompactRole;
 import java.util.List;
 
-public record ProjectMember(String user, List<CompactRole> roles) {
+public record ProjectMember(String user, long userId, List<CompactRole> roles) {
 }
