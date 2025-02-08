7b319c0419d84228643d111f341b06633c9c3e6a
MiniDigger | Martin <admin@minidigger.dev>
2025-02-08 15:38:13 +0100

    fix: pinned version downloads

--- backend/src/main/java/io/papermc/hangar/db/dao/internal/versions/HangarVersionsDAO.java
+++ backend/src/main/java/io/papermc/hangar/db/dao/internal/versions/HangarVersionsDAO.java
@@ -17,30 +17,68 @@ public interface HangarVersionsDAO {
     // TODO fixup view and this
     @SqlQuery("""
         (SELECT pv.version_id,
-               pv."type",
-               pv.version_string AS name,
-               pc.name pc_name,
-               pc.description pc_description,
-               pc.created_at pc_created_at,
-               pc.color pc_color,
-               pc.flags pc_flags
-        FROM pinned_versions pv
-            JOIN project_versions p ON pv.version_id = p.id
-            JOIN project_channels pc ON pc.id = p.channel_id
-        WHERE pv.project_id = :projectId AND pv.type = 'version' AND p.visibility = 0 LIMIT 5)
+                pv.version_string,
+                pv."type",
+                pv.version_string                                                                        AS name,
+                pc.name                                                                                     pc_name,
+                pc.description                                                                              pc_description,
+                pc.created_at                                                                               pc_created_at,
+                pc.color                                                                                    pc_color,
+                pc.flags                                                                                    pc_flags,
+                (SELECT ARRAY [p.owner_name, p.slug] FROM projects p WHERE p.id = pv.project_id LIMIT 1) AS project_namespace,-- needed for downloads
+                (SELECT json_agg(json_build_object('file_size', pvd.file_size,
+                                                   'hash', pvd.hash,
+                                                   'file_name', pvd.file_name,
+                                                   'external_url', pvd.external_url,
+                                                   'platforms', pvd.platforms,
+                                                   'download_platform', pvd.download_platform)) AS value
+                 FROM project_version_downloads pvd
+                 WHERE pvd.version_id = pv.version_id
+                 GROUP BY pvd.version_id)                                                                AS downloads,
+                (SELECT json_agg(json_build_object(
+                    'platform', plv.platform,
+                    'version', plv.version)) AS value
+                 FROM project_version_platform_dependencies pvpd
+                     JOIN platform_versions plv ON pvpd.platform_version_id = plv.id
+                 WHERE pvpd.version_id = pv.version_id)                                                  AS platform_dependencies,
+                'dum'                                                                                    AS platform_dependencies_formatted
+         FROM pinned_versions pv
+             JOIN project_versions p ON pv.version_id = p.id
+             JOIN project_channels pc ON pc.id = p.channel_id
+         WHERE pv.project_id = :projectId AND pv.type = 'version' AND p.visibility = 0
+         LIMIT 5)
         UNION ALL
         (SELECT pv.version_id,
-               pv."type",
-               pv.version_string AS name,
-               pc.name pc_name,
-               pc.description pc_description,
-               pc.created_at pc_created_at,
-               pc.color pc_color,
-               pc.flags pc_flags
-        FROM pinned_versions pv
-            JOIN project_versions p ON pv.version_id = p.id
-            JOIN project_channels pc ON pc.id = p.channel_id
-        WHERE pv.project_id = :projectId AND pv.type = 'channel' AND p.visibility = 0 LIMIT 1)
+                pv.version_string,
+                pv."type",
+                pv.version_string                                                                        AS name,
+                pc.name                                                                                     pc_name,
+                pc.description                                                                              pc_description,
+                pc.created_at                                                                               pc_created_at,
+                pc.color                                                                                    pc_color,
+                pc.flags                                                                                    pc_flags,
+                (SELECT ARRAY [p.owner_name, p.slug] FROM projects p WHERE p.id = pv.project_id LIMIT 1) AS project_namespace,-- needed for downloads
+                (SELECT json_agg(json_build_object('file_size', pvd.file_size,
+                                                   'hash', pvd.hash,
+                                                   'file_name', pvd.file_name,
+                                                   'external_url', pvd.external_url,
+                                                   'platforms', pvd.platforms,
+                                                   'download_platform', pvd.download_platform)) AS value
+                 FROM project_version_downloads pvd
+                 WHERE pvd.version_id = pv.version_id
+                 GROUP BY pvd.version_id)                                                                AS downloads,
+                (SELECT json_agg(json_build_object(
+                    'platform', plv.platform,
+                    'version', plv.version)) AS value
+                 FROM project_version_platform_dependencies pvpd
+                     JOIN platform_versions plv ON pvpd.platform_version_id = plv.id
+                 WHERE pvpd.version_id = pv.version_id)                                                  AS platform_dependencies,
+                'dum'                                                                                    AS platform_dependencies_formatted
+         FROM pinned_versions pv
+             JOIN project_versions p ON pv.version_id = p.id
+             JOIN project_channels pc ON pc.id = p.channel_id
+         WHERE pv.project_id = :projectId AND pv.type = 'channel' AND p.visibility = 0
+         LIMIT 1)
         """)
     @RegisterConstructorMapper(HangarProject.PinnedVersion.class)
     List<HangarProject.PinnedVersion> getPinnedVersions(long projectId);
