b82c3a2feaa04ab4ee186fe25df07db555578400
MiniDigger | Martin <admin@minidigger.dev>
2025-04-19 11:38:18 +0200

    chore: add some logging to index service

--- backend/src/main/java/io/papermc/hangar/components/index/IndexService.java
+++ backend/src/main/java/io/papermc/hangar/components/index/IndexService.java
@@ -2,11 +2,17 @@ package io.papermc.hangar.components.index;
 
 import io.papermc.hangar.model.api.project.Project;
 import io.papermc.hangar.model.api.project.version.Version;
+import java.time.Duration;
+import java.time.LocalDateTime;
 import java.util.List;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 @Service
 public class IndexService {
+
+    private static final Logger logger = LoggerFactory.getLogger(IndexService.class);
 
     private final IndexDAO indexDAO;
     private final MeiliService meiliService;
@@ -17,10 +23,14 @@ public class IndexService {
     }
 
     public void fullUpdateProjects() {
+        LocalDateTime now = LocalDateTime.now();
+        logger.info("Starting full update projects");
         List<Project> projects = this.indexDAO.getAllProjects("");
+        logger.info("Got {} projects after {}s", projects.size(), Duration.between(now, LocalDateTime.now()).toSeconds());
         this.meiliService.setupProjectIndex("-new");
         this.meiliService.waitForTask(this.meiliService.sendDocuments(MeiliService.PROJECT_INDEX + "-new", projects));
         this.meiliService.swapIndexes(MeiliService.PROJECT_INDEX, MeiliService.PROJECT_INDEX + "-new");
+        logger.info("Finished full update projects after {}s", Duration.between(now, LocalDateTime.now()).toSeconds());
     }
 
     public void updateProject(long id) {
@@ -33,10 +43,14 @@ public class IndexService {
     }
 
     public void fullUpdateVersions() {
+        LocalDateTime now = LocalDateTime.now();
+        logger.info("Starting full update versions");
         List<Version> versions = this.indexDAO.getAllVersions("");
+        logger.info("Got {} versions after {}s", versions.size(), Duration.between(now, LocalDateTime.now()).toSeconds());
         this.meiliService.setupVersionIndex("-new");
         this.meiliService.waitForTask(this.meiliService.sendDocuments(MeiliService.VERSION_INDEX + "-new", versions));
         this.meiliService.swapIndexes(MeiliService.VERSION_INDEX, MeiliService.VERSION_INDEX + "-new");
+        logger.info("Finished full update versions after {}s", Duration.between(now, LocalDateTime.now()).toSeconds());
     }
 
     public void updateVersion(long id) {
