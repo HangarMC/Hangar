ba175f49918ea55152f49159cead5b56c8f8c10f
MiniDigger | Martin <admin@minidigger.dev>
2025-04-23 08:17:06 +0200

    chore: try to fix pnpm file?

--- frontend/.pnpmfile.cjs
+++ frontend/.pnpmfile.cjs
@@ -1,6 +1,7 @@
 module.exports = {
   hooks: {
     readPackage(pkg) {
+      // unused and brings deprecated transitive dependencies
       if (pkg?.dependencies?.["@netlify/functions"]) {
         delete pkg.dependencies["@netlify/functions"];
       }
